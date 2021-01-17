ddd80dcfa021c536ab5599f21da7c231adf453e9
Konrad Jamrozik <kojamroz@microsoft.com>
2021-01-16 18:17:00 -0800

    ongoing

--- wikitools/wikitools/src/Program.cs
+++ wikitools/wikitools/src/Program.cs
@@ -22,15 +22,6 @@ namespace Wikitools
             var gitLog = GitLog(os, cfg.GitRepoClonePath, cfg.GitExecutablePath, cfg.GitLogDays);
             var wiki   = Wiki(adoApi, cfg.AdoWikiUri, cfg.AdoPatEnvVar, cfg.AdoWikiPageViewsForDays);
 
-            var gitAuthorsReport = new GitAuthorsStatsReport(timeline, gitLog, cfg.GitLogDays);
-            var gitFilesReport   = new GitFilesStatsReport(timeline, gitLog, cfg.GitLogDays);
-            var pageViewsReport  = new PagesViewsStatsReport(timeline, wiki, cfg.AdoWikiPageViewsForDays);
-            var wikiDigest       = new WikiDigest(gitAuthorsReport, gitFilesReport, pageViewsReport);
-            // kja core functionality, temp commented out while experimenting
-            //await wikiDigest.WriteAsync(Console.Out);
-
-            // kja return table of monthly wiki contributions, sum of insertions per month, MoM % change
-
             // Obtain inputs. Has out-of-process dependencies.
             List<GitAuthorChangeStats>       authorsChangesStats        = await gitLog.GetAuthorChangesStats();
             List<GitFileChangeStats>         filesChangesStats          = await gitLog.GetFileChangesStats(); 
@@ -51,24 +42,23 @@ namespace Wikitools
 
         private static List<List<GitAuthorChangeStats>> GetMonthlyAuthorsChangesStats(GitLog gitLog)
         {
+            /*
+            reportData = 
+            {
+            Description = "Wiki insertions by month, from MM-DD-YYYY to MM-DD-YYYY, both inclusive."
+            TabularData = new TabularData(
+              HeaderRow = Month, Insertions, Monthly Change
+              Rows = 
+                List<List<GitFileChangeStats>> monthly change lists = foreach <month start and end dates>: GitLog.GetFilesChanges(start month, end month)
+                monthly insertions sums = foreach monthly change list: sum insertions
+                aggregate: monthly insertion sums: add MoM change. 
+                transpose aggregate: instead of two lists (List(sums), List(MoM)), make it List(Month(sum), Month(MoM)) (see https://morelinq.github.io/3.0/ref/api/html/M_MoreLinq_MoreEnumerable_Transpose__1.htm)
+            )
+            }
+            */
             // kja to implement
             return new();
         }
     }
 }
 
-/*
-
-reportData = 
-{
-Description = "Wiki insertions by month, from MM-DD-YYYY to MM-DD-YYYY, both inclusive."
-TabularData = new TabularData(
-  HeaderRow = Month, Insertions, Monthly Change
-  Rows = 
-    List<List<GitFileChangeStats>> monthly change lists = foreach <month start and end dates>: GitLog.GetFilesChanges(start month, end month)
-    monthly insertions sums = foreach monthly change list: sum insertions
-    aggregate: monthly insertion sums: add MoM change. 
-    transpose aggregate: instead of two lists (List(sums), List(MoM)), make it List(Month(sum), Month(MoM)) (see https://morelinq.github.io/3.0/ref/api/html/M_MoreLinq_MoreEnumerable_Transpose__1.htm)
-)
-}
-*/
