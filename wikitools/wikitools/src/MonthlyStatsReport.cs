32a7fb0c7952a0219ac95e5fdd3814244c87a57d
Konrad Jamrozik <kojamroz@microsoft.com>
2021-01-17 13:05:31 -0800

    ongoing

--- wikitools/wikitools/src/MonthlyStatsReport.cs
+++ wikitools/wikitools/src/MonthlyStatsReport.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using MoreLinq;
 using Wikitools.Lib.Git;
 using Wikitools.Lib.Primitives;
 
 namespace Wikitools
 {
-    // kja NEXT
     public record MonthlyStatsReport(ITimeline Timeline, GitLogCommit[] Commits) : MarkdownDocument
     {
         public override List<object> Content =>
             new()
             {
-                $"Git file insertions month over month",
+                $"Git file insertions and deletions month over month",
                 "",
                 new TabularData2(GetRows(Commits))
             };
@@ -22,27 +19,27 @@ namespace Wikitools
         {
             var commitsByMonth = commits
                 .Where(commit => !commit.Author.Contains("Konrad J"))
-                .GroupBy(commit => (commit.Date.Month + (commit.Date.Year-2019)*100));
+                .GroupBy(commit => $"{commit.Date.Year} {commit.Date.Month}");
 
-            // kja temp debug
-            // foreach (var month in commitsByMonth)
-            // {
-            //     foreach (var c in month)
-            //     {
-            //         Console.Out.WriteLine($"{c.Author} {c.Date.ToShortDateString()} {c.Stats.Sum(s => s.Insertions)}");
-            //     }
-            // }
+            static bool FilePathFilter(GitLogCommit.Numstat stat) => !stat.FilePath.Contains("/Meta");
 
             var insertionsByMonth = commitsByMonth.Select(month => (
-                month: month.Key,
-                insertions: month.Sum(commit => commit.Stats
-                            .Where(stat => !stat.FilePath.Contains("/Meta"))
-                            .Sum(stat => stat.Insertions))));
+                    month: month.Key,
+                    insertions: month.Sum(c => c.Stats.Where(FilePathFilter).Sum(ns => ns.Insertions)),
+                    deletions: month.Sum(c => c.Stats.Where(FilePathFilter).Sum(ns => ns.Deletions))
+                )
+            );
+
             var rows = insertionsByMonth
-                .Select(monthData => new object[] { monthData.month, monthData.insertions, "TODO MoM change" })
+                .Select(monthData => new object[]
+                {
+                    monthData.month,
+                    monthData.insertions,
+                    monthData.deletions
+                })
                 .ToArray();
 
-            return (headerRow: new object[] { "Month", "Insertions", "MoM change" }, rows);
+            return (headerRow: new object[] { "Month", "Insertions", "Deletions" }, rows);
         }
     }
 }
