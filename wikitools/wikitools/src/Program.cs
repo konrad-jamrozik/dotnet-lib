417de51abc0abd4e13690f29f0d1aff344d60a1e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-01-17 14:00:08 -0800

    ongoing

--- wikitools/wikitools/src/Program.cs
+++ wikitools/wikitools/src/Program.cs
@@ -18,32 +18,31 @@ namespace Wikitools
             var os       = new WindowsOS();
             var adoApi   = new AdoApi();
 
+            // kja dehardcode the variables below
+            var cfgStartYear = new DateTime(2019, 1, 1);
+            var cfgEndYear   = new DateTime(2020, 1, 1);
+            // kja use in all reports, dehardcode "/Meta". Same with "Konrad J" filter.
+            Func<string, bool> filePathFilter = filePath => !filePath.Contains("/Meta");
+
             var gitLog = GitLog(os, cfg.GitRepoClonePath, cfg.GitExecutablePath, cfg.GitLogDays);
             var wiki   = Wiki(adoApi, cfg.AdoWikiUri, cfg.AdoPatEnvVar, cfg.AdoWikiPageViewsForDays);
 
             // Obtain inputs. Has out-of-process dependencies.
-            List<GitAuthorChangeStats> authorsChangesStats = await gitLog.GetAuthorChangesStats(cfg.GitLogDays);
-            List<GitFileChangeStats>   filesChangesStats   = await gitLog.GetFileChangesStats();
-            List<WikiPageStats>        pagesViewsStats     = await wiki.GetPagesStats();
+            GitLogCommit[]      recentCommits   = await GetCommits(gitLog, cfg.GitLogDays);
+            GitLogCommit[]      allCommits      = await GetCommits(gitLog, cfgStartYear, cfgEndYear);
+            List<WikiPageStats> pagesViewsStats = await wiki.GetPagesStats();
 
-            GitLogCommit[] recentCommits = await GetCommits(gitLog, cfg.GitLogDays);
-
-            // kja dehardcode dates
-            GitLogCommit[] allCommits = await GetCommits(gitLog, new DateTime(2019, 1, 1), new DateTime(2020, 1, 1));
-
-            // kja use in all reports, dehardcode "/Meta". Same with "Konrad J" filter.
-            Func<string, bool> filePathFilter = filePath => !filePath.Contains("/Meta");
-            var authorsReport = new GitAuthorsStatsReport2(timeline, cfg.GitLogDays, recentCommits);
-            var filesReport = new GitFilesStatsReport2(timeline, cfg.GitLogDays, recentCommits, filePathFilter);
+            var authorsReport    = new GitAuthorsStatsReport2(timeline, cfg.GitLogDays, recentCommits);
+            var filesReport      = new GitFilesStatsReport2(timeline, cfg.GitLogDays, recentCommits, filePathFilter);
             var pagesViewsReport = new PagesViewsStatsReport2(timeline, cfg.AdoWikiPageViewsForDays, pagesViewsStats);
-            var monthlyReport = new MonthlyStatsReport(timeline, allCommits, filePathFilter);
+            var monthlyReport    = new MonthlyStatsReport(timeline, allCommits, filePathFilter);
 
             // Write outputs. Side-effectful.
             // kja temp off
             await authorsReport.WriteAsync(Console.Out);
             await filesReport.WriteAsync(Console.Out);
-            // await pagesViewsReport.WriteAsync(Console.Out);
-            // await monthlyReport.WriteAsync(Console.Out);
+            await pagesViewsReport.WriteAsync(Console.Out);
+            await monthlyReport.WriteAsync(Console.Out);
         }
 
         // kja this should be GitLog method
