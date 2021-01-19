03c2b477ab970f7fc0bf863a86643f3abfafce1e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-01-18 18:28:49 -0800

    lay out in pseudocode next tasks

--- wikitools/azuredevops/src/WikiPageStats.cs
+++ wikitools/azuredevops/src/WikiPageStats.cs
@@ -12,7 +12,7 @@ namespace Wikitools.AzureDevOps
             GetStats(pageDetail)) { }
 
         private static Stat[] GetStats(WikiPageDetail pageDetail) =>
-            pageDetail.ViewStats?.Select(dayStat => new Stat(dayStat.Count, dayStat.Day)).ToArray()
+            pageDetail.ViewStats?.Select(dayStat => new Stat(dayStat.Count, dayStat.Day.ToUniversalTime())).ToArray()
             ?? Array.Empty<Stat>();
 
         public record Stat(int Count, DateTime Day) { }
--- wikitools/wikitools/src/WikiPagesStatsStorage.cs
+++ wikitools/wikitools/src/WikiPagesStatsStorage.cs
@@ -22,13 +22,41 @@ namespace Wikitools
                     WriteIndented = true
                 });
 
-            var path       = Path.Join(StorageDirPath, $"date_{DateTime.UtcNow:yyy_MM_dd}.json");
-            var storageDir = new Dir(OS.FileSystem, path);
-            await File.WriteAllTextAsync(storageDir.Path, serialized);
+            // kja NEXT curr work.
+            // Proven to be able to write to file system. 
+            // Next tasks:
+            // - replace File.WriteAllTextAsync. Introduce File abstraction or similar,
+            // that depends on OS.FileSystem. Make it create Dirs as needed when writing out.
+            // possibly pass it as Options param, like CreateMissingDirs.
+            // - implement logic as follows, in pseudocode:
+            //
+            //   var persistedStats = new PersistedWikiStats(storageDir);
+            //   var lastDate = await persistedStats.LastPersistedDate; // need to await sa it reads from FileSystem
+            //   var pageStats = await wiki.PagesStats(DateTime.now - lastDate)
+            //   var (lastMonthStats, thisMonthStats) = pageStats.SplitByMonth()
+            //   await new MergedStats(lastMonthStats, persistedStats.LastMonth).Persist();
+            //   await new MergedStats(thisMonthStats, persistedStats.ThisMonth).Persist();
+            //   // internally, the two above will deserialize the persisted JSON into WikiPageStats,
+            //   // then merge with ___monthStats WikiPageStats, and then serialize back to the file system.
+            //
+            // Later: think about decoupling the logic from FileSystem; maybe arbitrary storage via streams/writers
+            // would make more sense. At least the merging and spliting algorithm should be decoupled from file system.
+            var storageDir = new Dir(OS.FileSystem, StorageDirPath);
+            if (!storageDir.Exists())
+                Directory.CreateDirectory(storageDir.Path);
+            var filePath = Path.Join(StorageDirPath, $"date_{DateTime.UtcNow:yyy_MM_dd}.json");
+            await File.WriteAllTextAsync(filePath, serialized);
 
             return this;
         }
 
+        // kja to implement, as follows, in pseudocode:
+        //   var persistedStats = new PersistedWikiStats(storageDir);
+        //   var lastMonthDays = ...; var thisMonthDays = ...
+        //   return
+        //     persistedStats.LastMonth.FilterTo(lastMonthDays)
+        //     .Union
+        //       persistedStats.ThisMonth.FilterTo(thisMonthDays)
         public Task<WikiPageStats[]> PagesStats(int pageViewsForDays) 
             => Wiki.PagesStats(pageViewsForDays);
     }
