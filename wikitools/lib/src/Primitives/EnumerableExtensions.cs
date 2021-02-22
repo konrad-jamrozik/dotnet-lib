74871467d58b4d4cc80f2cf97c8a903814e13250
Konrad Jamrozik <kojamroz@microsoft.com>
2021-02-21 21:35:01 -0800

    refactor IEnumerable extensions

--- wikitools/lib/src/Primitives/EnumerableExtensions.cs
+++ wikitools/lib/src/Primitives/EnumerableExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using MoreLinq;
 using Wikitools.Lib.Contracts;
 
 namespace Wikitools.Lib.Primitives
@@ -10,9 +9,9 @@ namespace Wikitools.Lib.Primitives
     {
         public static (TGroupKey key, TSource[] items)[] GroupAndOrderBy<TSource, TGroupKey>(
             this IEnumerable<TSource> source,
-            Func<TSource, TGroupKey> keySelector)
+            Func<TSource, TGroupKey> selectKey)
         {
-            ILookup<TGroupKey, TSource>             lookup        = source.ToLookup(keySelector);
+            ILookup<TGroupKey, TSource>             lookup        = source.ToLookup(selectKey);
             IEnumerable<(TGroupKey Key, TSource[])> groups        = lookup.Select(g => (g.Key, g.ToArray()));
             (TGroupKey Key, TSource[])[]            orderedGroups = groups.OrderBy(t => t.Key).ToArray();
             return orderedGroups;
@@ -21,87 +20,28 @@ namespace Wikitools.Lib.Primitives
         public static TSource[] UnionUsing<TSource, TKey>(
             this IEnumerable<TSource> first,
             IEnumerable<TSource> second,
-            Func<TSource, TKey> keySelector,
-            Func<TSource, TSource, TSource> intersector) where TKey : struct
+            Func<TSource, TKey> selectKey,
+            Func<TSource, TSource, TSource> intersect) where TKey : struct
         {
             var firstArray  = first as TSource[] ?? first.ToArray();
             var secondArray = second as TSource[] ?? second.ToArray();
 
-            var firstByKey  = firstArray.ToDictionary(keySelector);
-            var secondByKey = secondArray.ToDictionary(keySelector);
+            var firstByKey  = firstArray.ToDictionary(selectKey);
+            var secondByKey = secondArray.ToDictionary(selectKey);
 
-            var firstKeySet      = Enumerable.ToHashSet(firstArray.Select(keySelector));
-            var secondKeySet     = Enumerable.ToHashSet(secondArray.Select(keySelector));
-            var intersectingKeys = Enumerable.ToHashSet(firstKeySet.Intersect(secondKeySet));
+            var firstKeySet      = firstArray.Select(selectKey).ToHashSet();
+            var secondKeySet     = secondArray.Select(selectKey).ToHashSet();
+            var intersectingKeys = firstKeySet.Intersect(secondKeySet).ToHashSet();
 
-            var firstExceptSecond = firstKeySet.Except(secondKeySet).Select(key => firstByKey[key]); // kja test for this .Except
+            var firstExceptSecond =
+                firstKeySet.Except(secondKeySet).Select(key => firstByKey[key]); // kja test for this .Except
             var secondExceptFirst = secondKeySet.Except(firstKeySet).Select(key => secondByKey[key]);
-            var intersecting      = intersectingKeys.Select(key => intersector(firstByKey[key], secondByKey[key]));
+            var intersecting      = intersectingKeys.Select(key => intersect(firstByKey[key], secondByKey[key]));
 
             var union = firstExceptSecond.Union(intersecting).Union(secondExceptFirst).ToArray();
 
-            Contract.Assert(union.DistinctBy(keySelector).Count() == union.Length, "Any given key appears only once");
+            Contract.Assert(union.DistinctBy(selectKey).Count() == union.Length, "Any given key appears only once");
             return union;
-        }
-
-        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
-            this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector) => MoreEnumerable.DistinctBy(source, keySelector);
-
-        public static void ForEach<TSource>(
-            this IEnumerable<TSource> source,
-            Action<TSource> action) => MoreEnumerable.ForEach(source, action);
-
-        public static IExtremaEnumerable<TSource> MaxBy<TSource, TKey>(
-            this IEnumerable<TSource> source,
-            Func<TSource, TKey> selector) => MoreEnumerable.MaxBy(source, selector);
-
-        public static void AssertDistinctBy<TSource, TKey>(
-            this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector)
-        {
-            var sourceArray = source as TSource[] ?? source.ToArray();
-            if (MoreEnumerable.DistinctBy(sourceArray, keySelector).Count() != sourceArray.Length)
-            {
-                throw new InvariantException();
-            }
-        }
-
-        public static void AssertOrderedBy<TSource, TKey>(
-            this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector)
-        {
-            var sourceArray = source as TSource[] ?? source.ToArray();
-            var ordered     = sourceArray.OrderBy(keySelector).ToArray();
-            if (!ordered.SequenceEqual(sourceArray))
-            {
-                throw new InvariantException();
-            }
-        }
-
-        public static void Assert<TSource>(
-            this IEnumerable<TSource> source,
-            Func<TSource, bool> predicate)
-        {
-            try
-            {
-                MoreEnumerable.Assert(source, predicate);
-            }
-            catch (Exception e)
-            {
-                throw new InvariantException("MoreEnumerable.Assert threw", e);
-            }
-        }
-
-        public static void AssertSingleBy<TSource, TKey>(
-            this IEnumerable<TSource> source,
-            Func<TSource, TKey> keySelector,
-            string message)
-        {
-            if (MoreEnumerable.DistinctBy(source, keySelector).Count() != 1)
-            {
-                throw new InvariantException(message);
-            }
         }
     }
 }
---
+++ wikitools/lib/src/Primitives/EnumerableMoreLinqExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+using Wikitools.Lib.Contracts;
+
+namespace Wikitools.Lib.Primitives
+{
+    public static class EnumerableMoreLinqExtensions
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selectKey) => MoreEnumerable.DistinctBy(source, selectKey);
+
+        public static void ForEach<TSource>(
+            this IEnumerable<TSource> source,
+            Action<TSource> action) => MoreEnumerable.ForEach(source, action);
+
+        public static IExtremaEnumerable<TSource> MaxBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selectKey) => MoreEnumerable.MaxBy(source, selectKey);
+
+        public static void AssertDistinctBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selectKey)
+        {
+            var sourceArray = source as TSource[] ?? source.ToArray();
+            if (MoreEnumerable.DistinctBy(sourceArray, selectKey).Count() != sourceArray.Length)
+            {
+                throw new InvariantException();
+            }
+        }
+
+        public static void AssertOrderedBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selectKey)
+        {
+            var sourceArray = source as TSource[] ?? source.ToArray();
+            var ordered     = sourceArray.OrderBy(selectKey).ToArray();
+            if (!ordered.SequenceEqual(sourceArray))
+            {
+                throw new InvariantException();
+            }
+        }
+
+        public static void Assert<TSource>(
+            this IEnumerable<TSource> source,
+            Func<TSource, bool> predicate)
+        {
+            try
+            {
+                MoreEnumerable.Assert(source, predicate);
+            }
+            catch (Exception e)
+            {
+                throw new InvariantException("MoreEnumerable.Assert threw", e);
+            }
+        }
+
+        public static void AssertSingleBy<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> selectKey,
+            string message)
+        {
+            if (MoreEnumerable.DistinctBy(source, selectKey).Count() != 1)
+            {
+                throw new InvariantException(message);
+            }
+        }
+    }
+}
