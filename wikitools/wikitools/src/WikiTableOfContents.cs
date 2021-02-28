f5625f66a6bc26ffcb3dacb834435c23e11ab640
Konrad Jamrozik <kojamroz@microsoft.com>
2021-02-27 18:28:57 -0800

    laying out in comments the data types for Wikitools.WikiTableOfContents.GetContent

--- wikitools/wikitools/src/WikiTableOfContents.cs
+++ wikitools/wikitools/src/WikiTableOfContents.cs
@@ -11,6 +11,18 @@ namespace Wikitools
         {
             // kja current work
             // Build tree of relative paths, indented by depth
+            // Input:
+            // DataTree<WikiTocEntry>
+            // - enumerable by lexicographically sorted DFS, i.e. in the order it should
+            //   be printed out.
+            // WikiTocEntry: (int depth, WikiPagePath path, WikiPageStats stats)
+            // - depth will be used to compute indent
+            // - WikiPagePath can be enumerated for each components
+            //   - this report knows how to convert path to hyperlink
+            //     - hyperlink conversion probably should be abstracted to be generic
+            // - stats will be used to compute if icons should show: new, active, stale
+            //  - thresholds for icons passed separately as param, coming from config
+            // - this report knows how to print out wikiTocEntry
             return data;
         }
     }
