9ef60ec324972d3ff479fc95c64f103b6ff36913
Konrad Jamrozik <kojamroz@microsoft.com>
2021-03-06 13:36:23 -0800

    work on Trie

--- wikitools/lib/src/Data/Trie.cs
+++ wikitools/lib/src/Data/Trie.cs
@@ -1,29 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Wikitools.Lib.Primitives;
 
 namespace Wikitools.Lib.Data
 {
     // kja wire this in, instead of TreeData
     public abstract record Trie<TValue>(IEnumerable<PathPrefix<TValue>> Prefixes)
     {
-        public IEnumerable<(IList<string> Path, TValue value)> AsPreorderEnumerable() => PreorderTraversal();
+        public IEnumerable<PathPrefix<TValue>> PreorderTraversal() => PreorderTraversal(Prefixes);
 
-        private IEnumerable<(IList<string> Path, TValue value)> PreorderTraversal()
-        {
-            IEnumerable<(IList<string> Path, TValue value)> preorderTree = PreorderTraversal(Prefixes);
-            return preorderTree;
-        }
+        private static IEnumerable<PathPrefix<TValue>> PreorderTraversal(IEnumerable<PathPrefix<TValue>> prefixes) =>
+            prefixes.SelectMany(PreorderTraversal);
 
-        private static IEnumerable<(IList<string> Path, TValue value)> PreorderTraversal(IEnumerable<PathPrefix<TValue>> prefixes) =>
-            prefixes.SelectMany(PreorderTraversal).ToList();
-
-        private static IEnumerable<(IList<string> Path, TValue value)> PreorderTraversal(PathPrefix<TValue> prefix)
-        {
-            var currPrefix = new List<(IList<string> Path, TValue value)> { (prefix.PathSegments, prefix.Value) };
-            var suffixes = prefix.PathSuffixes.Any()
-                ? PreorderTraversal(prefix.PathSuffixes)
-                : new List<(IList<string> Path, TValue value)>();
-            return currPrefix.Concat(suffixes).ToList();
-        }
+        private static IEnumerable<PathPrefix<TValue>> PreorderTraversal(PathPrefix<TValue> prefix) =>
+            prefix.WrapInList().Concat(
+                prefix.PathSuffixes.Any()
+                    ? PreorderTraversal(prefix.PathSuffixes)
+                    : new List<PathPrefix<TValue>>());
     }
 }
