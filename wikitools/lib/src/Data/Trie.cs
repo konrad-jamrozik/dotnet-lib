d8dbb34224914d66cbe06ced9508b85c7a1ab296
Konrad Jamrozik <kojamroz@microsoft.com>
2021-03-06 21:43:55 -0800

    ongoing

--- wikitools/lib/src/Data/Trie.cs
+++ wikitools/lib/src/Data/Trie.cs
@@ -18,11 +18,11 @@ namespace Wikitools.Lib.Data
                 // kja in general, options like that:
                 // - return PathParts that have nonempty Suffixes, or not
                 // - split each PathParts into a sequence of PathParts, each with one segment, or not.
-                // - for each returned PathPart, include all the segments from all the preceeding path parts, or not.
+                // - for each returned PathPart, include all the segments from all the preceding path parts, or not.
                 // For wiki I can say the following:
                 // - I need each PathPart with nonempty suffix, as it might be a page or dir with subpages
                 // - I need path each segment independently, as segment is a page or dir
-                // - I need all the preceeding segments, so I can construct full URL to the page.
+                // - I need all the preceding segments, so I can construct full URL to the page.
 
                 part.Suffixes.Any()
                     ? PreorderTraversal(part.Suffixes)
