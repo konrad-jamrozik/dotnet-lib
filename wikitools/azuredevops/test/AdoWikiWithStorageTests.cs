cdd01887f44a46e2b90571153786d2ee1bdc323e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-03-14 21:11:02 -0700

    ongoin

--- wikitools/azuredevops/test/AdoWikiWithStorageTests.cs
+++ wikitools/azuredevops/test/AdoWikiWithStorageTests.cs
@@ -8,21 +8,12 @@ namespace Wikitools.AzureDevOps.Tests
 {
     public class AdoWikiWithStorageTests
     {
-        // kja integration test:
-        // - Obtain 10 days from wiki (days 1 to 10)
-        // - Obtain 5 days from wiki (days 6 to 10)
-        // - Save to storage days 3 to 7
-        // - Obtain 5 days from "wiki with storage"
-        // - Assert the storage now has days 3 to 10
-
         // kja simulated tests
         // - Everything empty: no data from wiki, no data from storage
         // - Data in wiki, nothing in storage
         // - Storage with data from 3 months
-
-        // kja curr work: ObtainsDataFromAdoApiAndStorage
         [Fact]
-        public async Task ObtainsDataFromAdoApiAndStorage()
+        public async Task NoData()
         {
             var wikiPagesStats = new WikiPageStats[] {};
 
@@ -42,13 +33,43 @@ namespace Wikitools.AzureDevOps.Tests
             var pagesStats = await wiki.PagesStats(pageViewsForDays);
         }
 
+        // kja integration test:
+        // - Obtain 10 days from wiki (days 1 to 10)
+        // - Obtain 5 days from wiki (days 6 to 10)
+        // - Save to storage days 3 to 7
+        // - Obtain 5 days from "wiki with storage"
+        // - Assert the storage now has days 3 to 10
+        [Trait("category", "integration")]
+        [Fact]
+        public async Task ObtainsDataFromAdoApiAndStorage()
+        {
+            var pageViewsForDays = 30;
+            
+            var windowsOS  = new WindowsOS();
+            var fileSystem = windowsOS.FileSystem;
+            var timeline   = new Timeline();
+            var adoApi     = new AdoApi(windowsOS.Environment);
+
+            // kja circular dependency: azuredevops-tests should not depend on wikitools
+            var cfg = WikitoolsConfig.From(fileSystem, "wikitools_config.json");
+
+            var storageDirPath = cfg.StorageDirPath;
+            var adoWikiUri     = cfg.AdoWikiUri;
+            var patEnvVar      = cfg.AdoPatEnvVar;
+
+            var wiki = AdoWikiWithStorage(adoApi, adoWikiUri, patEnvVar, fileSystem, storageDirPath, timeline);
+
+            // Act
+            var pagesStats = await wiki.PagesStats(pageViewsForDays);
+        }
+
         private static AdoWikiWithStorage AdoWikiWithStorage(
-            SimulatedAdoApi adoApi,
+            IAdoApi adoApi,
             string adoWikiUri,
             string patEnvVar,
             IFileSystem fileSystem,
             string storageDirPath,
-            SimulatedTimeline timeline)
+            ITimeline timeline)
         {
             var adoWiki = new AdoWiki(adoApi, new AdoWikiUri(adoWikiUri), patEnvVar);
             var storage = new WikiPagesStatsStorage(
