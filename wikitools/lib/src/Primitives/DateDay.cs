141449113bb9448d7b9600c7a91a8569f96b9cae
Konrad Jamrozik <kojamroz@microsoft.com>
2021-03-16 02:21:03 -0700

    add DateDay, in preparation of fixing .Trim()

---
+++ wikitools/lib/src/Primitives/DateDay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Wikitools.Lib.Primitives
+{
+    public record DateDay(int Year, int Month, int Day) : IComparable<DateDay>, IComparable<DateTime>, IEquatable<DateTime>
+    {
+        public DateDay(DateTime dateTime) : this(dateTime.Year, dateTime.Month, dateTime.Day) { }
+
+        private readonly DateTime _dateTime = new(Year, Month, Day);
+
+        public int CompareTo(DateTime other) => _dateTime.CompareTo(other);
+
+        public int CompareTo(DateDay? other) => _dateTime.CompareTo(other?._dateTime);
+
+        public virtual bool Equals(DateTime other) => _dateTime.Equals(other);
+
+        public virtual bool Equals(DateDay? other) => _dateTime.Equals(other?._dateTime);
+
+        public override int GetHashCode() => _dateTime.GetHashCode();
+    }
+}
