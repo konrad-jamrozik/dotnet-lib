3954bc74e07e69c83c06eed4b89c999f5211e873
Konrad Jamrozik <kojamroz@microsoft.com>
2021-05-14 03:20:08 -0700

    work on config

--- wikitools/lib/src/Json/IConfiguration.cs
+++ wikitools/lib/src/Json/IConfiguration.cs
@@ -1,3 +1,4 @@
+using System;
 using Wikitools.Lib.Contracts;
 
 namespace Wikitools.Lib.Json
@@ -5,6 +6,8 @@ namespace Wikitools.Lib.Json
     public interface IConfiguration
     {
         public const string ConfigSuffix = "Cfg";
+
+        public static string FileName(Type cfg) => FileName(cfg.Name);
 
         public static string FileName(string cfgName)
         {
--- wikitools/lib/test/Json/ConfigurationTests.cs
+++ wikitools/lib/test/Json/ConfigurationTests.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using MoreLinq;
 using NUnit.Framework;
 using Wikitools.Lib.Json;
@@ -7,6 +6,7 @@ using Wikitools.Lib.OS;
 
 namespace Wikitools.Lib.Tests.Json
 {
+    // kja 8 curr TDD for 7: tests for Read() without params, including reading multiple configs.
     [TestFixture]
     public class ConfigurationTests
     {
@@ -16,15 +16,13 @@ namespace Wikitools.Lib.Tests.Json
 
         private record CompositeCfg(string Qux, LeafCfg? LeafCfg) : IConfiguration;
 
-        [Test]
-        public void ReadsEmptyConfig() => Verify(new EmptyCfg());
+        [Test] public void ReadsEmptyConfigNew() => VerifyNewReadSingle(new EmptyCfg());
+
+        [Test] public void ReadsLeafConfigNew() => VerifyNewReadSingle(new LeafCfg("fooVal", new[] { 1, 2 }));
 
         [Test]
-        public void ReadsLeafConfig() => Verify(new LeafCfg("fooVal", new[] { 1, 2 }));
-
-        [Test]
-        public void ReadsCompositeConfig() =>
-            Verify(new CompositeCfg("fooVal", new LeafCfg("quxVal", new[] { 1, 2 })));
+        public void ReadsCompositeConfigNew() =>
+            VerifyNewReadSingle(new CompositeCfg("fooVal", new LeafCfg("quxVal", new[] { 1, 2 })));
 
         [Test]
         public void ComposesAndReadsCompositeConfig()
@@ -32,22 +30,35 @@ namespace Wikitools.Lib.Tests.Json
             var leafCfg           = new LeafCfg("fooVal", new[] { 1, 2 });
             var compositeCfg      = new CompositeCfg("quxVal", leafCfg);
             var compositeCfgShell = new CompositeCfg("quxVal", null);
-            
-            // kja 8 curr TDD for 7 The composite config shall be composed from two files
-            // Pass key value pairs denoting the two config files with simpleCfg and compositeCfg. Their names and contents.
+
             var cfgs = new Dictionary<string, IConfiguration>
             {
-                [IConfiguration.FileName(nameof(LeafCfg))] = leafCfg,
-                [IConfiguration.FileName(nameof(CompositeCfg))] = compositeCfgShell
+                [IConfiguration.FileName(typeof(LeafCfg))] = leafCfg,
+                [IConfiguration.FileName(typeof(CompositeCfg))] = compositeCfgShell
             };
-            Verify2(cfgs, compositeCfg);
+            VerifyNewRead(cfgs, compositeCfg);
         }
 
-        private void Verify2<TConfig>(Dictionary<string, IConfiguration> cfgs, TConfig expectedCfg) where TConfig : IConfiguration
+        [Test] public void ReadsEmptyConfig() => VerifyOldRead(new EmptyCfg());
+
+
+        [Test] public void ReadsLeafConfig() => VerifyOldRead(new LeafCfg("fooVal", new[] { 1, 2 }));
+
+        [Test]
+        public void ReadsCompositeConfig() =>
+            VerifyOldRead(new CompositeCfg("fooVal", new LeafCfg("quxVal", new[] { 1, 2 })));
+
+
+        private void VerifyNewReadSingle<TConfig>(TConfig expectedCfg) where TConfig : IConfiguration
         {
             var fs = new SimulatedFileSystem();
 
-            cfgs.ForEach(kvp => fs.CurrentDir.WriteAllTextAsync(kvp.Key, kvp.Value.ToJsonIndentedUnsafe()));
+            var cfgs = new Dictionary<string, IConfiguration>
+            {
+                [IConfiguration.FileName(typeof(TConfig))] = expectedCfg
+            };
+
+            cfgs.ForEach(kvp => fs.CurrentDir.WriteAllTextAsync(kvp.Key, kvp.Value.ToJsonUnsafe(ignoreNulls: true)));
 
             // Act
             TConfig actualCfg = new Configuration(fs).Read<TConfig>();
@@ -55,9 +66,22 @@ namespace Wikitools.Lib.Tests.Json
             new JsonDiffAssertion(expectedCfg, actualCfg).Assert();
         }
 
-        private void Verify<TConfig>(TConfig cfg) where TConfig : IConfiguration
+        private void VerifyNewRead<TConfig>(Dictionary<string, IConfiguration> cfgs, TConfig expectedCfg)
+            where TConfig : IConfiguration
         {
             var fs = new SimulatedFileSystem();
+
+            cfgs.ForEach(kvp => fs.CurrentDir.WriteAllTextAsync(kvp.Key, kvp.Value.ToJsonUnsafe(ignoreNulls: true)));
+
+            // Act
+            TConfig actualCfg = new Configuration(fs).Read<TConfig>();
+
+            new JsonDiffAssertion(expectedCfg, actualCfg).Assert();
+        }
+
+        private void VerifyOldRead<TConfig>(TConfig cfg) where TConfig : IConfiguration
+        {
+            var fs          = new SimulatedFileSystem();
             var cfgFileName = "config.json";
 
             fs.CurrentDir.WriteAllTextAsync(cfgFileName, cfg.ToJsonUnsafe(ignoreNulls: true));
