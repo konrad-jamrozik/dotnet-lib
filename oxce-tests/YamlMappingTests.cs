640f4dd33714ce9d992589bbcfc8b53244a0ffc3
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-09 22:12:36 -0800

    minor

--- oxce-tests/YamlMappingTests.cs
+++ oxce-tests/YamlMappingTests.cs
@@ -15,17 +15,17 @@ namespace OxceTests
                     "key2:  ",
                     "  - foo",
                     "  - bar",
-                    "abcdef: 42",
+                    "entry3: 42",
                     "key 4: - qux"
                 });
 
             Assert.Multiple(() =>
             {
                 // Acts
-                Assert.That(yamlMapping.Lines("key1"), Is.EquivalentTo(new[] { "5" }));
-                Assert.That(yamlMapping.Lines("key2"), Is.EquivalentTo(new[] { "- foo", "- bar" }));
-                Assert.That(yamlMapping.Lines("abcdef"), Is.EquivalentTo(new[] {"42"}));
-                Assert.That(yamlMapping.Lines("key 4"), Is.EquivalentTo(new[] {"- qux"}));
+                Assert.That(yamlMapping.Lines("key1"),   Is.EquivalentTo(new[] { "5" }));
+                Assert.That(yamlMapping.Lines("key2"),   Is.EquivalentTo(new[] { "- foo", "- bar" }));
+                Assert.That(yamlMapping.Lines("entry3"), Is.EquivalentTo(new[] { "42" }));
+                Assert.That(yamlMapping.Lines("key 4"),  Is.EquivalentTo(new[] { "- qux" }));
 
             });
         }
