f8a6279345d2e8a191ef1794b7f3c446a539c4ff
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-15 22:45:34 -0800

    fix todos

--- wikitools/WikiTableOfContents.cs
+++ wikitools/WikiTableOfContents.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Wikitools.AzureDevOps;
 using Wikitools.Lib.Markdown;
-using Wikitools.Lib.OS;
 using Wikitools.Lib.Primitives;
 
 namespace Wikitools
@@ -44,25 +43,57 @@ namespace Wikitools
             AdoWikiPagesPaths pagesPaths,
             Task<IEnumerable<WikiPageStats>> pagesStatsTask)
         {
-            // kj2 document the Separator replace. Its ASCII ord is 47 while e.g. space is 32, which led to weird sorting of paths.
-            var pagesStats = (await pagesStatsTask).OrderBy(ps => ps.Path.Replace(WikiPageStatsPath.Separator, " "));
-            var convertedFilePaths = pagesPaths
+            var pagesStats = SortPaths(await pagesStatsTask);
+            var wikiPathsFromFsPaths = pagesPaths
                 .Select(path => (string)WikiPageStatsPath.FromFileSystemPath(path))
                 .OrderBy(p => p.Replace(WikiPageStatsPath.Separator, " "));
 
-            var lines = convertedFilePaths.ZipMatching(
+            var lines = wikiPathsFromFsPaths.ZipMatching(
                 pagesStats,
-                match: (fsPath, wikiPageStats)
-                    => fsPath == wikiPageStats.Path,
-                selectResult: (pathPart, wikiPageStats)
-                    // kja here the path in () needs to be converted to wiki path format: spaces to dashes, dashes to %2D, () to \(\).
+                match: (wikiPath, wikiPageStats)
+                    => wikiPath == wikiPageStats.Path,
+                selectResult: (wikiPath, wikiPageStats)
                     => $"[{wikiPageStats.Path}]({ConvertPathToWikiLink(wikiPageStats.Path)}) - {wikiPageStats.DayStats.Sum(ds => ds.Count)} views");
             return lines.Cast<object>().ToArray();
         }
 
+        private static IOrderedEnumerable<WikiPageStats> SortPaths(IEnumerable<WikiPageStats> pagesStats)
+        {
+            return pagesStats
+                // Here we replace separator with space so that space doesn't interfere with sorting of directories.
+                // ASCII ordinal of the separator is 47, while of space it is 32,
+                // so by replacing the separator with the space,
+                // space will no longer be sorted first.
+                //
+                // Without this custom sorting, instead of ending up with paths sorted like this:
+                //
+                // /dirFoo
+                // /dirFoo/file_A1
+                // /dirFoo/file_A2
+                // /dirFoo Bar/file_B1
+                // /dirFoo Bar/file_B2
+                //
+                // We would end up with paths sorted like this:
+                //
+                // /dirFoo
+                // /dirFoo Bar/file_B1
+                // /dirFoo Bar/file_B2
+                // /dirFoo/file_A1
+                // /dirFoo/file_A2
+                // 
+                .OrderBy(ps => ps.Path.Replace(WikiPageStatsPath.Separator, " "));
+        }
+
+        /// <summary>
+        /// Converts the 'path', which is in format Wikitools.AzureDevOps.WikiPageStatsPath.WikiPageStatsPath,
+        /// to a link that ADO Wiki understands as an absolute path to a page in given wiki.
+        ///
+        /// Reference:
+        /// See "Supported links for Wiki" / "Absolute path of Wiki pages" in:
+        /// <a href="https://docs.microsoft.com/en-us/azure/devops/project/wiki/markdown-guidance?view=azure-devops#links" />
+        /// </summary>
         private static string ConvertPathToWikiLink(string path)
         {
-            // kja 5 refactor
             path = path.Replace("-","%2D").Replace(" ","-").Replace("(", @"\(").Replace(")", @"\)");
             return path;
         }
