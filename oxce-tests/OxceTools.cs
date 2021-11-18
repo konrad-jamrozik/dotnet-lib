447498b65fb9fd366f2a3573f1b2b0bc89f7dd8e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-17 20:14:20 -0800

    minor fixup

--- oxce-tests/OxceTools.cs
+++ oxce-tests/OxceTools.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using NUnit.Framework;
 using Wikitools.Lib.Json;
 using Wikitools.Lib.OS;
