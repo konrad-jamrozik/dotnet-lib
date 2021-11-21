1e93c3cd7e37554588545c4022aaae85c89cc891
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-21 06:16:14 -0800

    work on WritesTableOfContentsFromLocalWikiGitClone

--- wikitools-tests/WikiTableOfContentsIntegrationTests.cs
+++ wikitools-tests/WikiTableOfContentsIntegrationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Wikitools.AzureDevOps;
@@ -31,23 +32,36 @@ namespace Wikitools.Tests
                 fs,
                 cfg.TestStorageDirPath,
                 timeline.UtcNow);
-            // kja when the pagesStats input goes beyond what is stored on file system, no exception is thrown, which is not great.
+            // kj2 when the pagesStats input goes beyond what is stored on file system, no exception is thrown, which is not great.
             var pagesStats = wiki.PagesStats(90).Result;
 
             var toc = new WikiTableOfContents(wikiPagesPaths, Task.FromResult(pagesStats));
 
-            // Act
-            var contentResult = toc.Content.Result;
+            // kja 2 introduce abstraction for writing to temporary output test dir. Something like:
+            // var testFile = new TemporaryTestFile(cfg.TestStorageDirPath);
+            // // Act
+            // var lines = testFile.Write(writable /* : IWritableToText */)
+            var outputFilePath = cfg.TestStorageDirPath + Path.DirectorySeparatorChar +
+                                 nameof(WritesTableOfContentsFromLocalWikiGitClone) + ".txt";
+
+            // kja 2 should use fs
+            using (var outputFileWriter = File.CreateText(outputFilePath))
+            {
+                // Act
+                toc.WriteAsync(outputFileWriter).Wait();
+
+            }
+
+            Console.Out.WriteLine($"Wrote to {outputFilePath}");
+
+            // kja 2 should use fs
+            var actualLines = File.ReadAllLines(outputFilePath);
 
             Assert.That(
-                contentResult.Length, 
+                actualLines.Length, 
                 Is.GreaterThanOrEqualTo(cfg.TestGitRepoExpectedPathsMinPageCount));
 
-            for (var i = 0; i < contentResult.Length; i++)
-            {
-                // kja 5 make proper test assertion. Probably some count based on config file.
-                Console.Out.WriteLine(contentResult[i] + "  ");
-            }
+            // kja 2 add assertion over min views count from cfg. This will need to show how to read back the data from file.
         }
     }
 }
