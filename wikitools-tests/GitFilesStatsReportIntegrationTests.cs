896a5337525c439c4cd75dc97ac911e52c7f361e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-24 04:36:48 -0800

    add WritesFilesStatsReport

--- wikitools-tests/GitFilesStatsReportIntegrationTests.cs
+++ wikitools-tests/GitFilesStatsReportIntegrationTests.cs
@@ -1,5 +1,5 @@
-using NUnit.Framework;
 using System.Linq;
+using NUnit.Framework;
 using Wikitools.AzureDevOps;
 using Wikitools.Lib.Json;
 using Wikitools.Lib.OS;
@@ -10,44 +10,43 @@ namespace Wikitools.Tests;
 
 [Category("integration")]
 [TestFixture]
-public class GitAuthorsStatsReportIntegrationTests
+public class GitFilesStatsReportIntegrationTests
 {
     [Test]
-    public void WritesAuthorsStatsReport()
+    public void WritesFilesStatsReport()
     {
         var fs = new FileSystem();
         var cfg = new Configuration(fs).Read<WikitoolsIntegrationTestsCfg>();
-        var authorsReport = GitAuthorsStatsReport(fs, cfg.WikitoolsCfg);
+        var filesReport = GitAuthorsStatsReport(fs, cfg.WikitoolsCfg);
         var testFile = new TestFile(cfg.TestStorageDir(fs));
 
         // Act
-        var lines = testFile.Write(authorsReport);
+        var lines = testFile.Write(filesReport);
 
         Assert.That(lines.Length, Is.GreaterThanOrEqualTo(3));
         Assert.That(lines.Count(l => l.StartsWith("| ")), Is.GreaterThanOrEqualTo(3));
     }
 
-    private static GitAuthorsStatsReport GitAuthorsStatsReport(
+    private static GitFilesStatsReport GitAuthorsStatsReport(
         IFileSystem fs,
         WikitoolsCfg cfg)
     {
         var timeline = new Timeline();
         var os = new WindowsOS();
-        bool AuthorFilter(string author) => !cfg.ExcludedAuthors.Any(author.Contains);
-
+        bool PathFilter(string path) => !cfg.ExcludedPaths.Any(path.Contains);
         var gitLogDecl = new GitLogDeclare();
         var gitLog = gitLogDecl.GitLog(
             os,
             cfg.GitRepoCloneDir(fs),
             cfg.GitExecutablePath);
         var recentCommits = gitLog.Commits(cfg.GitLogDays);
-        var authorsReport = new GitAuthorsStatsReport(
+        var filesReport = new GitFilesStatsReport(
             timeline,
             recentCommits,
             cfg.GitLogDays,
             cfg.Top,
-            AuthorFilter);
+            PathFilter);
 
-        return authorsReport;
+        return filesReport;
     }
 }
