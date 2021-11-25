b4ef299761f1d1505692f967c80eb36ad9710ec3
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-24 19:19:15 -0800

    simplify GitAuthorsStatsReport

--- wikitools/GitAuthorsStatsReport.cs
+++ wikitools/GitAuthorsStatsReport.cs
@@ -18,7 +18,10 @@ namespace Wikitools
             int days,
             int? top = null,
             Func<string, bool>? authorFilter = null) : base(
-            GetContent(timeline, days, commits, authorFilter ?? (_ => true), top)) { }
+            GetContent(
+                timeline,
+                days,
+                GitAuthorStats.AuthorsStatsFrom(commits.Result, authorFilter ?? (_ => true), top))) { }
 
         public GitAuthorsStatsReport(
             ITimeline timeline,
@@ -33,34 +36,6 @@ namespace Wikitools
                 "",
                 new TabularData(Rows(stats))
             };
-
-        private static async Task<object[]> GetContent(
-            ITimeline timeline,
-            int days,
-            Task<GitLogCommit[]> commits,
-            Func<string, bool> authorFilter,
-            int? top) =>
-            new object[]
-            {
-                string.Format(DescriptionFormat, days, timeline.UtcNow),
-                "",
-                new TabularData(Rows(await commits, authorFilter, top))
-            };
-
-        private static (object[] headerRow, object[][] rows) Rows(
-            GitLogCommit[] commits,
-            Func<string, bool> authorFilter,
-            int? top)
-        {
-            GitAuthorStats[] rows = GitAuthorStats.AuthorsStatsFrom(commits, authorFilter, top);
-
-            // kj2 Rows conversion to object[]: instead of this conversion, TabularData should
-            // handle not only object[][], but also arbitrary_record[], and use reflection
-            // to convert this record into a an array of objects[].
-            var rowsAsObjectArrays = rows.Select(GitAuthorStats.AsObjectArray).ToArray();
-
-            return (headerRow: GitAuthorStats.HeaderRow, rowsAsObjectArrays);
-        }
 
         private static (object[] headerRow, object[][] rows) Rows(
             GitAuthorStats[] rows)
