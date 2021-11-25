4ebae1b2173670de5c6931730cd2d5fb96a3569e
Konrad Jamrozik <kojamroz@microsoft.com>
2021-11-25 04:08:47 -0800

    add note on prepping TopStats

--- wikitools/TopStatsReport.cs
+++ wikitools/TopStatsReport.cs
@@ -6,11 +6,11 @@ namespace Wikitools;
 // kja TopStatsReport
 // implement "top stats" report, which will show:
 // - top 10 most edited pages last week. Might be less than 10 if not enough activity.
-//   - Data will come from GitFilesStatsReport
-//     - Data already there (GitFileStats), but need to tweak filtering
+//   - Data already there (GitFileStats), but need to tweak filtering
 // - top 10 most viewed pages last week. Might be less than 10 if not enough activity.
-//   - Data will come from GitPagesViewsReport
+//   - Data already here (GitPathViewStats), but need to tweak filtering
 // - Same as above, but for the last month.
+//   - Need to add it to report
 // - Same as above, but for top 3 most active authors (with exclusions)
 //   - Data already here (GitAuthorStats), but need to tweak filtering
 // - Add annotations (icons): Newly added, lots of traffic (use :fire: in the MD)
@@ -24,6 +24,12 @@ public record TopStatsReport : MarkdownDocument
     public const string PathViewDescriptionFormat =
         "Path views since last {0} days as of {1}. Total wiki pages: {2}";
 
+    // kja the report itself decides what are the top ranges (7d, 30d, top 10), so it needs to
+    // to do the filtering itself, and not get the data as input.
+    // UNLESS the data should come from config, like "TopStatsReportSettings" json element.
+    // Then this report just unpacks a bundle of stats, like "TopStats", which has "TopStats.AuthorStatsForLastWeek".
+    // Yeah, I think I will do the "TopStats as input" approach, but perhaps the 7d/30d/top 10 won't 
+    // necessarily come from config; it will be hardcoded in the TopStats type itself.
     public TopStatsReport(
         ITimeline timeline,
         int days,
