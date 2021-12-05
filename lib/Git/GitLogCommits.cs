79f8da00a5fd3da4679e53f52ab911bc994fdc15
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-05 06:40:28 -0800

    add GitLogCommits and DaySpan

---
+++ lib/Git/GitLogCommits.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wikitools.Lib.Primitives;
+
+namespace Wikitools.Lib.Git
+{
+    public record GitLogCommits : IEnumerable<GitLogCommit>
+    {
+        private readonly IEnumerable<GitLogCommit> _commits;
+
+        public readonly DaySpan DaySpan;
+
+        public GitLogCommits(IEnumerable<GitLogCommit> commits, DaySpan daySpan)
+        {
+            _commits = commits.Where(commit => daySpan.Contains(commit.Date));
+            DaySpan = daySpan;
+        }
+
+        public IEnumerator<GitLogCommit> GetEnumerator()
+            => _commits.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
---
+++ lib/Primitives/DaySpan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Wikitools.Lib.Primitives
+{
+    public record DaySpan(DateDay SinceDay, DateDay UntilDay)
+    {
+        public bool Contains(DateTime date)
+            => SinceDay.CompareTo(date) <= 0 && 0 <= UntilDay.CompareTo(date);
+    }
+}
