62497e9280f1ee32942bcc656dddb7642e43f7ab
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-15 18:25:18 -0800

    added RankedTop

---
+++ lib/Data/RankedTop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wikitools.Lib.Data;
+
+// kja use it in TopStatsReport
+public record RankedTop<T>(IEnumerable<T> Seq, int? Top) : IEnumerable<(int rank, T data)>
+{
+    private IEnumerable<(int rank, T data)> RankedTopSeq
+        => Seq.Take(Top != null ? new Range(0, (int)Top) : Range.All)
+            .Select((elem, rank) => (rank + 1, elem));
+    
+    public IEnumerator<(int rank, T data)> GetEnumerator()
+        => RankedTopSeq.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => GetEnumerator();
+}
