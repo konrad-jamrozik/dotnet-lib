05dbd4cfcc43e2cf6bb718d2dc82ff314041992c
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-15 18:26:16 -0800

    rename

--- lib/Data/RankedTop.cs
+++ lib/Data/RankedTop.cs
@@ -6,13 +6,13 @@ using System.Linq;
 namespace Wikitools.Lib.Data;
 
 // kja use it in TopStatsReport
-public record RankedTop<T>(IEnumerable<T> Seq, int? Top) : IEnumerable<(int rank, T data)>
+public record RankedTop<T>(IEnumerable<T> Seq, int? Top) : IEnumerable<(int rank, T elem)>
 {
-    private IEnumerable<(int rank, T data)> RankedTopSeq
+    private IEnumerable<(int rank, T elem)> RankedTopSeq
         => Seq.Take(Top != null ? new Range(0, (int)Top) : Range.All)
             .Select((elem, rank) => (rank + 1, elem));
     
-    public IEnumerator<(int rank, T data)> GetEnumerator()
+    public IEnumerator<(int rank, T elem)> GetEnumerator()
         => RankedTopSeq.GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator()
