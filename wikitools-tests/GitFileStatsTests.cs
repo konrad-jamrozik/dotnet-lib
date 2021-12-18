2bdb8508ed3cbd3c36f8ea38634f004140992ae3
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-18 15:36:03 -0800

    lay out in pseudocode algorithm for supporting renames

--- wikitools-tests/GitFileStatsTests.cs
+++ wikitools-tests/GitFileStatsTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace Wikitools.Tests;
 
-// kja curr work
+// kja curr test
 public class GitFileStatsTests
 {
     [Fact]
@@ -26,11 +26,19 @@ public class GitFileStatsTests
                 {
                     new GitLogCommit.Numstat(6, 2, "/abc/def/bar.md")
                 }),
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(0, 0, "/abc/def/{bar.md => qux.md}")
+                }),
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(100, 400, "/abc/def/qux.md")
+                }),
             });
 
         Assert.Single(stats);
-        Assert.Equal("/abc/def/bar.md", stats[0].FilePath);
-        Assert.Equal(36, stats[0].Insertions);
-        Assert.Equal(12, stats[0].Deletions);
+        Assert.Equal("/abc/def/qux.md", stats[0].FilePath);
+        Assert.Equal(136, stats[0].Insertions);
+        Assert.Equal(412, stats[0].Deletions);
     }
 }
--- wikitools/GitFileStats.cs
+++ wikitools/GitFileStats.cs
@@ -40,6 +40,37 @@ public record GitFileStats(
         var fileStats = commits.SelectMany(
             c => c.Stats.Select(s => (s.FilePath, s.Insertions, s.Deletions)));
         var statsByFilePath = fileStats.GroupBy(s => s.FilePath);
+        // kja curr work: support renames here
+        // Find all renames
+        // If given file path is a 'before' name in any of the renames,
+        // then add it to the 'after' group
+        //
+        // Consider case:
+        // a -> b
+        // b -> c
+        //
+        // Here the algorithm will first add a to b. Then it would find rename
+        // from b to c, adding b to c, resulting in abc in one group.
+        //
+        // Optimization idea:
+        //
+        // Come up with chains like "abc ends up being d", "gh ends up being i".
+        // Then go another groupBy on the existing groupBy, where they would be
+        // grouped on the new property "finalName".
+        //
+        // So, algorithm 2:
+        //
+        // 1. Go through all the renames, build the chain of renames.
+        // 2. For each existing group, add the new property of final name;
+        // 3. Then group by that new property.
+        //
+        // Generalization of algorithm 2:
+        //
+        // 1. Function that takes as input a list of pairs: (string from, string to).
+        //    Returns: a hashmap: {key: string from, value: string finalTo }
+        // 2. Function that takes as input a hashmap {key: string, value: enumerable }
+        //    Returns: hashmap, where all the enumerables having the same finalTo
+        //    are merged into one, under the "finalTo" key.
         var statsSumByFilePath = statsByFilePath.Select(
             pathStats => new GitFileStats(
                 pathStats.Key,
