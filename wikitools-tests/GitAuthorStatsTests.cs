0a012ef2d9d5c68cab81c77dad67c6ba5a3dcf93
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-19 17:14:50 -0800

    add test capturing wrong author stats on file rename

--- wikitools-tests/GitAuthorStatsTests.cs
+++ wikitools-tests/GitAuthorStatsTests.cs
@@ -15,21 +15,21 @@ public class GitAuthorStatsTests
             {
                 new GitLogCommit("Author1", now, new[]
                 {
-                    new GitLogCommit.Numstat(30, 10, "/foo.txt")
+                    new GitLogCommit.Numstat(30, 10, "foo.txt")
                 }),
                 new GitLogCommit("Author2", now, new[]
                 {
-                    new GitLogCommit.Numstat(30, 10, "/foo.txt"),
-                    new GitLogCommit.Numstat(30, 10, "/bar.txt")
+                    new GitLogCommit.Numstat(30, 10, "foo.txt"),
+                    new GitLogCommit.Numstat(30, 10, "bar.txt")
                 }),
                 new GitLogCommit("Author1", now, new[]
                 {
-                    new GitLogCommit.Numstat(30, 10, "/foo.txt")
+                    new GitLogCommit.Numstat(30, 10, "foo.txt")
                 }),
                 new GitLogCommit("Author2", now, new[]
                 {
-                    new GitLogCommit.Numstat(30, 10, "/foo.txt"),
-                    new GitLogCommit.Numstat(30, 10, "/qux.txt")
+                    new GitLogCommit.Numstat(30, 10, "foo.txt"),
+                    new GitLogCommit.Numstat(30, 10, "qux.txt")
                 }),
             });
 
@@ -37,4 +37,36 @@ public class GitAuthorStatsTests
         Assert.Equal(1, stats[0].FilesChanges);
         Assert.Equal(3, stats[1].FilesChanges);
     }
+
+    [Fact] // kja curr test
+    public void SumsByAuthorRenamedFile()
+    {
+        var now = new SimulatedTimeline().UtcNow;
+        var stats = GitAuthorStats.SumByAuthor(
+            new[]
+            {
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(100, 6, "abc/def/foo.txt")
+                }),
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(200, 5, "abc/{def => ghi}/foo.txt")
+                }),
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(300, 4, "abc/ghi/{foo => bar}.txt")
+                }),
+                new GitLogCommit("Author1", now, new[]
+                {
+                    new GitLogCommit.Numstat(400, 3, "abc/ghi/bar.txt")
+                }),
+            });
+
+        Assert.Single(stats);
+        Assert.Equal(1, stats[0].FilesChanges);
+        Assert.Equal(100+200+300+400, stats[1].Insertions);
+        Assert.Equal(6+5+4+3, stats[1].Deletions);
+    }
+
 }
