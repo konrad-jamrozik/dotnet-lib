9ccbfed5c544ed45d16cca88fb61a7ac3d281553
Konrad Jamrozik <kojamroz@microsoft.com>
2021-12-21 18:19:35 -0800

    dedup test data

--- wikitools-tests/GitFileStatsTests.cs
+++ wikitools-tests/GitFileStatsTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Wikitools.Lib.Git;
 using Wikitools.Lib.Primitives;
@@ -10,22 +11,16 @@ public class GitFileStatsTests
     [Fact]
     public void SumsByFilePathsWithSimpleRename()
     {
-        var now = new SimulatedTimeline().UtcNow;
-        var stats = GitFileStats.SumByFilePath(new[]
+        var data = new[]
         {
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(30, 10, "abc/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(3, 1, "abc/def/{foo.md => bar.md}")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(6, 2, "abc/def/bar.md")
-            })
-        }.Reverse());
+            (30, 10, "abc/def/foo.md"),
+            (3, 1, "abc/def/{foo.md => bar.md}"),
+            (6, 2, "abc/def/bar.md")
+        };
+        var commits = GitLogCommits(data);
+
+        // Act
+        var stats = GitFileStats.SumByFilePath(commits);
 
         Assert.Single(stats);
         Assert.Equal("abc/def/bar.md", stats[0].FilePath);
@@ -33,52 +28,39 @@ public class GitFileStatsTests
         Assert.Equal(10 + 1 + 2, stats[0].Deletions);
     }
 
-    // kja interleave in this tests also other rename chain
-    [Fact] 
+    [Fact]
     public void SumsByFilePathsWithRenamePresent()
     {
-        var now = new SimulatedTimeline().UtcNow;
-        var stats = GitFileStats.SumByFilePath(new[]
+        var data = new[]
         {
-            // kja compress into tuples. Same for test above.
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(100, 8, "abc/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(200, 7, "{ => intro}/abc/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(300, 6, "{intro/abc => }/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(400, 5, "{ => abc}/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(500, 4, "abc/{ => ghi/jkl}/def/foo.md"),
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(600, 3, "abc/ghi/jkl/def/foo.md")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(700, 2, "abc/ghi/jkl/def/{foo.md => qux.md}")
-            }),
-            new GitLogCommit("Author1", now, new[]
-            {
-                new GitLogCommit.Numstat(800, 1, "abc/ghi/jkl/def/qux.md")
-            }),
-        }.Reverse());
+            (100, 8, "abc/def/foo.md"),
+            (200, 7, "{ => intro}/abc/def/foo.md"),
+            (300, 6, "{intro/abc => }/def/foo.md"),
+            (400, 5, "{ => abc}/def/foo.md"),
+            (500, 4, "abc/{ => ghi/jkl}/def/foo.md"),
+            (600, 3, "abc/ghi/jkl/def/foo.md"),
+            (700, 2, "abc/ghi/jkl/def/{foo.md => qux.md}"),
+            (800, 1, "abc/ghi/jkl/def/qux.md")
+        };
+        var commits = GitLogCommits(data);
+
+        // Act
+        var stats = GitFileStats.SumByFilePath(commits);
 
         Assert.Single(stats);
         Assert.Equal("abc/ghi/jkl/def/qux.md", stats[0].FilePath);
-        Assert.Equal(100+200+300+400+500+600+700+800, stats[0].Insertions);
-        Assert.Equal(8+7+6+5+4+3+2+1, stats[0].Deletions);
+        Assert.Equal(100 + 200 + 300 + 400 + 500 + 600 + 700 + 800, stats[0].Insertions);
+        Assert.Equal(8 + 7 + 6 + 5 + 4 + 3 + 2 + 1, stats[0].Deletions);
+    }
 
+    private static IEnumerable<GitLogCommit> GitLogCommits((int, int, string)[] inputData)
+    {
+        var now = new SimulatedTimeline().UtcNow;
+        var data = inputData.Reverse().Select(
+            t => new GitLogCommit(
+                "Author1",
+                now,
+                new[] { new GitLogCommit.Numstat(t.Item1, t.Item2, t.Item3) }));
+        return data;
     }
 }
