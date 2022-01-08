f1dbd8ab6041e6362aec85310048ef030479ad2b
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-07 23:49:59 -0800

    add todos for Wikitools.Lib.Json.Configuration.ReadFromAssembly<TCfg>

--- lib/Json/Configuration.cs
+++ lib/Json/Configuration.cs
@@ -11,9 +11,16 @@ public record Configuration(IFileSystem FS)
 {
     // Implementation inspired by
     // https://stackoverflow.com/questions/465488/can-i-load-a-net-assembly-at-runtime-and-instantiate-a-type-knowing-only-the-na
+    // kja rethink the name of this metod
     public TCfg ReadFromAssembly<TCfg>() where TCfg : IConfiguration
     {
+        // kja the hardcoded paths here should come from a ctor param, in turn
+        // obtained from some top-level -config project, from a class
+        // that has hardcoded/inline bootstrapping config denoting from which paths load the
+        // remaining configs
         var currentDirectory = Directory.GetCurrentDirectory();
+        // kja the traversalToRepoParent can be merged with drill-down path below. And then
+        // the merged value should be passed as ctor param - see to-do above.
         // \dotnet-lib\wikitools-tests\bin\Debug\net6.0
         var traversalToRepoParent = @"..\..\..\..\..";
         var repoParentDir = Path.GetFullPath(Path.Join(currentDirectory, traversalToRepoParent));
