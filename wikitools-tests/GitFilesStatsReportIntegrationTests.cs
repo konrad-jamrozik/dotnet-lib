f56193db85fee21c4fc9fc0bde545634dc14f6e1
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-07 23:13:05 -0800

    WritesFilesStatsReport now uses the new config

--- wikitools-tests/GitFilesStatsReportIntegrationTests.cs
+++ wikitools-tests/GitFilesStatsReportIntegrationTests.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Wikitools.Config;
 using Wikitools.Lib;
 using Wikitools.Lib.Json;
 using Wikitools.Lib.OS;
@@ -17,8 +18,8 @@ public class GitFilesStatsReportIntegrationTests
     public async Task WritesFilesStatsReport()
     {
         var fs = new FileSystem();
-        var cfg = new Configuration(fs).Read<WikitoolsIntegrationTestsCfg>();
-        var filesReport = await GitFilesStatsReport(fs, cfg.WikitoolsCfg);
+        var cfg = new Configuration(fs).ReadFromAssembly<IWikitoolsIntegrationTestsCfg>();
+        var filesReport = await GitFilesStatsReport(fs, cfg.WikitoolsCfg());
         var testFile = new TestFile(cfg.TestStorageDir(fs));
 
         // Act
@@ -30,7 +31,7 @@ public class GitFilesStatsReportIntegrationTests
 
     private static async Task<GitFilesStatsReport> GitFilesStatsReport(
         IFileSystem fs,
-        WikitoolsCfg cfg)
+        IWikitoolsCfg cfg)
     {
         var timeline = new Timeline();
         var os = new WindowsOS();
@@ -40,13 +41,13 @@ public class GitFilesStatsReportIntegrationTests
             timeline,
             os,
             gitRepoDir,
-            cfg.GitExecutablePath);
+            cfg.GitExecutablePath());
 
-        var stats = await GitFileStats.From(gitLog, cfg.GitLogDays, cfg.ExcludedPaths);
+        var stats = await GitFileStats.From(gitLog, cfg.GitLogDays(), cfg.ExcludedPaths());
 
         var filesReport = new GitFilesStatsReport(
             timeline,
-            cfg.GitLogDays,
+            cfg.GitLogDays(),
             stats);
 
         return filesReport;
