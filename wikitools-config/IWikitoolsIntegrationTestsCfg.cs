335160a23f4c2d4de946ade0f3e95ca31e63ccd5
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-09 14:05:48 -0800

    ongoing

--- wikitools-config/IWikitoolsIntegrationTestsCfg.cs
+++ wikitools-config/IWikitoolsIntegrationTestsCfg.cs
@@ -8,13 +8,11 @@ public interface IWikitoolsIntegrationTestsCfg : IConfiguration
 {
     public IWikitoolsCfg WikitoolsCfg();
     public IAzureDevOpsTestsCfg AzureDevOpsTestsCfg();
-    public string TestStorageDirPath();
     public int TestGitRepoExpectedPathsMinPageCount();
     public int TestGitRepoExpectedPathsMinPageViewsCount();
 
     public IAzureDevOpsCfg AzureDevOpsCfg => AzureDevOpsTestsCfg().AzureDevOpsCfg();
 
-    // kja make it use AzureDevOpsTestsCfg.TestStorageDir.
-    // This will require for this class to use AzureDevOpsTestsCfg instead of AzureDevOpsCfg
-    public Dir TestStorageDir(IFileSystem fs) => new Dir(fs, TestStorageDirPath());
+    public Dir TestStorageDir(IFileSystem fs)
+        => new Dir(fs, AzureDevOpsTestsCfg().TestStorageDirPath());
 }
