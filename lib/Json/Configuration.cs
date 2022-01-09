cc6cf074b42995785ae2d5dbc98ef412baba5dfe
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-09 11:31:55 -0800

    refactor Configuration internals

--- lib/Json/Configuration.cs
+++ lib/Json/Configuration.cs
@@ -6,32 +6,35 @@ using Wikitools.Lib.OS;
 
 namespace Wikitools.Lib.Json;
 
-// kja use FS in the implementation
 public record Configuration(IFileSystem FS)
 {
+    private const string ConfigRepoCloneDirPath = "dotnet-lib-private";
+
+    private const string ConfigProjectName = "wikitools-configs";
+
+    private const string NetFrameworkVersion = "net6.0";
+
+    // The traversed directories are assumed to be:
+    // \<repo-dir>\<project-dir>\bin\<configuration>\<net-version>
+    private const string RelativeRepoPath = @"..\..\..\..\..";
+
+    private const string DllToLoadPath =
+        $@"{ConfigRepoCloneDirPath}\{ConfigProjectName}\bin\Debug\"
+        + $@"{NetFrameworkVersion}\{ConfigProjectName}.dll";
+
+    private const string LoadedClassNamespace = "Wikitools.Configs.";
+
     // Implementation inspired by
     // https://stackoverflow.com/questions/465488/can-i-load-a-net-assembly-at-runtime-and-instantiate-a-type-knowing-only-the-na
-    // kja rethink the name of this metod
+    // kja rethink the name of this method
     public TCfg ReadFromAssembly<TCfg>() where TCfg : IConfiguration
     {
-        // kja the hardcoded paths here should come from a ctor param, in turn
-        // obtained from some top-level -config project, from a class
-        // that has hardcoded/inline bootstrapping config denoting from which paths load the
-        // remaining configs
-        var currentDirectory = Directory.GetCurrentDirectory();
-        // kja the traversalToRepoParent can be merged with drill-down path below. And then
-        // the merged value should be passed as ctor param - see to-do above.
-        // \dotnet-lib\wikitools-tests\bin\Debug\net6.0
-        var traversalToRepoParent = @"..\..\..\..\..";
-        var repoParentDir = Path.GetFullPath(Path.Join(currentDirectory, traversalToRepoParent));
-        var dllPath = Path.Join(
-            repoParentDir,
-            @"dotnet-lib-private\wikitools-configs\bin\Debug\net6.0\wikitools-configs.dll");
+        var currentDirectory = FS.CurrentDir.Path;
+        var dllPath = Path.Join(currentDirectory, RelativeRepoPath, DllToLoadPath);
         Assembly assembly = Assembly.LoadFrom(dllPath);
         var interfaceName = typeof(TCfg).Name;
         var typeClassName = string.Concat(interfaceName.Skip(1));
-        Type type = assembly.GetType("Wikitools.Configs."+ typeClassName)!;
-
+        Type type = assembly.GetType(LoadedClassNamespace+ typeClassName)!;
         TCfg cfg = (TCfg)Activator.CreateInstance(type)!;
         return cfg;
     }
