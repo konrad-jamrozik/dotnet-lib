b8390ef4d35fbcf2cc9e511c5503d5e14af786a7
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-17 15:45:17 -0800

    refactor Soldier

--- oxce-tests/Soldier.cs
+++ oxce-tests/Soldier.cs
@@ -4,65 +4,71 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-namespace OxceTests
+// ReSharper disable NotAccessedPositionalProperty.Global
+// ReSharper disable UnusedMember.Global
+// Reason: all properties are accessed via the "Properties" member
+
+namespace OxceTests;
+
+public record Soldier(
+    int Id,
+    string Name,
+    string Type,
+    string BaseName,
+    int Missions,
+    int Kills,
+    int Rank,
+    int MonthsService,
+    float Recovery,
+    int ManaMissing,
+    int StatGainTotal,
+    int CurrentTU,
+    int CurrentStamina,
+    int CurrentHealth,
+    int CurrentBravery,
+    int CurrentReactions,
+    int CurrentFiring,
+    int CurrentThrowing,
+    int CurrentStrength,
+    int CurrentPsiStrength,
+    int CurrentPsiSkill,
+    int CurrentMelee,
+    int CurrentMana)
 {
-    public record Soldier(
-        int Id,
-        string Name,
-        string Type,
-        string BaseName,
-        int Missions,
-        int Kills,
-        int Rank,
-        int MonthsService,
-        float Recovery,
-        int ManaMissing,
-        int StatGainTotal,
-        int CurrentTU,
-        int CurrentStamina,
-        int CurrentHealth,
-        int CurrentBravery,
-        int CurrentReactions,
-        int CurrentFiring,
-        int CurrentThrowing,
-        int CurrentStrength,
-        int CurrentPsiStrength,
-        int CurrentPsiSkill,
-        int CurrentMelee,
-        int CurrentMana)
+    private static IEnumerable<PropertyInfo> Properties { get; } = typeof(Soldier).GetProperties();
+
+    public static string CsvHeaders()
+        => string.Join(",", 
+            Properties.Select(p => p.Name)
+                .Union(TrainingStats.CsvHeaders())
+                .Union(MaxStats.CsvHeaders()));
+
+    public override string ToString()
+        => $"{nameof(Soldier)} {{ "
+           + string.Join(", ", DataMap().Select(p => $"{p.Key} = {p.Value}")) + " }";
+
+    public string CsvString()
     {
-        public static string CsvHeaders()
-            => string.Join(",", 
-                Properties.Select(p => p.Name)
-                    .Union(TrainingStats.CsvHeaders())
-                    .Union(MaxStats.CsvHeaders()));
-
-        private static IEnumerable<PropertyInfo> Properties { get; } = typeof(Soldier).GetProperties();
-
-        public float KillsPerMission => Missions >= 5 ? (float) Kills / Missions : 0;
-
-        public int StatGainReal => Math.Max(StatGainTotal - CurrentPsiSkill, 0);
-
-        public string Helix => Name.EndsWith("H") ? "TRUE" : "FALSE";
-
-        public string Humanoid => Type is "STR_SOLDIER" or "STR_HYBRID" ? "TRUE" : "FALSE";
-
-        public override string ToString() => $"{nameof(Soldier)} {{ " 
-            + string.Join(", ", DataMap().Select(p => $"{p.Key} = {p.Value}")) + " }";
-
-        public string CsvString()
-        {
-            var str = ToString();
-            var csvStr = Regex.Replace(str, $"{nameof(Soldier)} {{(.*) }}", "$1");
-            csvStr = Regex.Replace(csvStr, " \\w+\\ = ", "");
-            return csvStr;
-        }
-
-        private IEnumerable<(string Key, object Value)> DataMap()
-        {
-            var propertyData = Properties.Select(p => (p.Name, p.GetValue(this)));
-            var allData = propertyData.Union(TrainingStats.Get(this)).Union(MaxStats.Get(this));
-            return allData;
-        }
+        var str = ToString();
+        var csvStr = Regex.Replace(str, $"{nameof(Soldier)} {{(.*) }}", "$1");
+        csvStr = Regex.Replace(csvStr, " \\w+\\ = ", "");
+        return csvStr;
     }
+
+    private IEnumerable<(string Key, object Value)> DataMap()
+    {
+        var propertyData = Properties.Select(p => (p.Name, p.GetValue(this)));
+        var allData = propertyData
+            .Union(TrainingStats.Get(this))
+            .Union(MaxStats.Get(this));
+        return allData;
+    }
+
+    public float KillsPerMission => Missions >= 5 ? (float) Kills / Missions : 0;
+
+    public int StatGainReal => Math.Max(StatGainTotal - CurrentPsiSkill, 0);
+
+    public string Helix => Name.EndsWith("H") ? "TRUE" : "FALSE";
+
+    public string Humanoid => Type is "STR_SOLDIER" or "STR_HYBRID" ? "TRUE" : "FALSE";
 }
