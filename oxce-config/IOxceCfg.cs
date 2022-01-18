347b9f2a7fbd12309480ef25cd657b0bfe654313
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-17 17:24:11 -0800

    ongoing refactoring

--- oxce-config/IOxceCfg.cs
+++ oxce-config/IOxceCfg.cs
@@ -1,23 +1,28 @@
 using Wikitools.Lib.Json;
+using Wikitools.Lib.OS;
 
 namespace Oxce.Configs;
 
 public interface IOxceCfg : IConfiguration
 {
     public string InputXcfSave();
-    public string OutputDirectory();
-    public string SoldierDataOutputFileName();
-    public string ItemCountsDataOutputFileName();
+    public string OutputDir();
+    public string SoldiersOutputFileName();
+    public string ItemCountsOutputFileName();
+
+    public string SoldiersOutputPath(IFileSystem fs) => fs.JoinPath(OutputDir(), SoldiersOutputFileName());
+
+    public string ItemCountsOutputPath(IFileSystem fs) => fs.JoinPath(OutputDir(), ItemCountsOutputFileName());
 
     public void Deconstruct(
         out string inputXcfSave,
         out string outputDirectory,
-        out string soldierDataOutputFileName,
-        out string itemCountsDataOutputFileName)
+        out string soldiersOutputFileName,
+        out string itemCountsOutputFileName)
     {
         inputXcfSave = InputXcfSave();
-        outputDirectory = OutputDirectory();
-        soldierDataOutputFileName = SoldierDataOutputFileName();
-        itemCountsDataOutputFileName = ItemCountsDataOutputFileName();
+        outputDirectory = OutputDir();
+        soldiersOutputFileName = SoldiersOutputFileName();
+        itemCountsOutputFileName = ItemCountsOutputFileName();
     }
 }
--- oxce-tests/OxceTools.cs
+++ oxce-tests/OxceTools.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Oxce.Configs;
 using Wikitools.Lib.Json;
 using Wikitools.Lib.OS;
-using Wikitools.Lib.Primitives;
 
 namespace OxceTests
 {
@@ -21,19 +17,15 @@ namespace OxceTests
         public async Task ProcessSaveFile()
         {
             var fs = new FileSystem();
-            var (inputXcfSave, outputDir, outputSoldiersFileName, outputItemCountsFileName) =
-                new Configuration(fs).Load<IOxceCfg>(
-                    configProjectName: "oxce-configs",
-                    loadedClassNamespace: "Oxce.Configs");
+            var cfg = new Configuration(fs).Load<IOxceCfg>(
+                configProjectName: "oxce-configs",
+                loadedClassNamespace: "Oxce.Configs");
 
-            var yamlMapping = new YamlMapping(fs.ReadAllLines(inputXcfSave));
+            var yamlMapping = new YamlMapping(fs.ReadAllLines(cfg.InputXcfSave()));
             var bases = Bases.FromSaveFile(yamlMapping);
 
-            var soldiersOutputPath = fs.JoinPath(outputDir, outputSoldiersFileName);
-            var itemCountsOutputPath = fs.JoinPath(outputDir, outputItemCountsFileName);
-
-            await bases.WriteSoldiers(fs, soldiersOutputPath);
-            await bases.WriteItemCounts(fs, itemCountsOutputPath);
+            await bases.WriteSoldiers(fs, cfg.SoldiersOutputPath(fs));
+            await bases.WriteItemCounts(fs, cfg.ItemCountsOutputPath(fs));
         }
 
 
