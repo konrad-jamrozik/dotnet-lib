3c527b52e841ad695c426a81e16fbeca3d3044e9
Konrad Jamrozik <kojamroz@microsoft.com>
2022-01-18 09:11:26 -0800

    oxce tests update

--- oxce-tests/OxceTools.cs
+++ oxce-tests/OxceTools.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Oxce.Configs;
@@ -16,18 +17,58 @@ namespace OxceTests
         [Test]
         public async Task ProcessSaveFile()
         {
+            await ParameterizedProcessSaveFile();
+        }
+
+        [Test]
+        public async Task ProcessSaveFileOld()
+        {
+            await ParameterizedProcessSaveFile(
+                "ufo landed.sav",
+                "soldiers - ufo landed.csv",
+                "item counts - ufo landed.csv");
+        }
+
+        [Test]
+        public async Task ProcessSaveFileNew()
+        {
+            await ParameterizedProcessSaveFile(
+                "ufo landed fixed reloaded.sav",
+                "soldiers - ufo landed fixed reloaded.csv",
+                "item counts - ufo landed fixed reloaded.csv");
+        }
+
+
+
+        private async Task ParameterizedProcessSaveFile(
+            string saveFileName = null,
+            string soldiersOutputFileName = null,
+            string itemCountsOutputFileName = null)
+        {
             var fs = new FileSystem();
             var cfg = new Configuration(fs).Load<IOxceCfg>(
                 configProjectName: "oxce-configs",
                 loadedClassNamespace: "Oxce.Configs");
 
-            var yamlMapping = new YamlMapping(fs.ReadAllLines(cfg.InputXcfSave()));
+            var saveFilePath = saveFileName != null
+                ? fs.JoinPath(cfg.SaveDir(), saveFileName)
+                : cfg.SaveFilePath(fs);
+
+            Console.Out.WriteLine("Reading " + saveFilePath);
+
+            var soldiersOutputPath = soldiersOutputFileName != null
+                ? fs.JoinPath(cfg.OutputDir(), soldiersOutputFileName)
+                : cfg.SoldiersOutputPath(fs);
+
+            var itemCountsOutputPath = itemCountsOutputFileName != null
+                ? fs.JoinPath(cfg.OutputDir(), itemCountsOutputFileName)
+                : cfg.ItemCountsOutputPath(fs);
+
+            var yamlMapping = new YamlMapping(fs.ReadAllLines(saveFilePath));
             var bases = Bases.FromSaveFile(yamlMapping);
 
-            await bases.WriteSoldiers(fs, cfg.SoldiersOutputPath(fs));
-            await bases.WriteItemCounts(fs, cfg.ItemCountsOutputPath(fs));
+            await bases.WriteSoldiers(fs, soldiersOutputPath);
+            await bases.WriteItemCounts(fs, itemCountsOutputPath);
         }
-
-
     }
 }
