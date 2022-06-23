729dc1b6729768e308b9dfd7ea181a5648fdacf6
Konrad Jamrozik <kojamroz@microsoft.com>
2022-06-22 21:29:02 -0700

    fix commendation scan perf

--- oxce-tests/CommendationBonuses.cs
+++ oxce-tests/CommendationBonuses.cs
@@ -3,34 +3,38 @@ using System.Linq;
 
 namespace OxceTests;
 
-public record CommendationBonuses(Commendations Commendations, SoldierBonuses SoldierBonuses)
+public record CommendationBonuses(
+    Dictionary<string, string[]> CommendationNameToSoldierBonusTypesMap,
+    Dictionary<string, SoldierBonus.StatsData> SoldierBonusNameToStatsMap)
 {
     public static CommendationBonuses Build(
         Commendations commendations,
         SoldierBonuses soldierBonuses)
-    {
-        return new CommendationBonuses(commendations, soldierBonuses);
-    }
+        => new CommendationBonuses(
+            commendations.ToDictionary(
+                commendation => commendation.Type,
+                commendation => commendation.SoldierBonusTypes),
+            soldierBonuses.ToDictionary(
+                soldierBonus => soldierBonus.Name,
+                soldierBonus => soldierBonus.Stats));
 
     public Soldier AddToSoldier(Soldier soldier)
     {
         // kja fixups to AddToSoldier:
-        // - improve perf: avoid doing .Single, instead, have a Dict
         // - get rid of foreachs: dedup, and convert to functional
         // - SoldierBonus.StatsData should be its own type, named Stats.
 
         var soldierBonuses = new List<SoldierBonus.StatsData>();
 
-        // Uncomment when perf fixed
-        // foreach (var (name, decoration) in soldier.Diary.Commendations)
-        // {
-        //     var nameWithoutNoun = name.Split("/").First();
-        //
-        //     var (_, soldierBonusTypes) = Commendations.CommendationData.Single(commendation => commendation.Type == nameWithoutNoun);
-        //     string soldierBonusType = soldierBonusTypes[decoration];
-        //     var (_, statsData) = SoldierBonuses.SoldierBonusData.Single(bonus => bonus.Name == soldierBonusType);
-        //     soldierBonuses.Add(statsData);
-        // }
+        foreach (var (name, decoration) in soldier.Diary.Commendations)
+        {
+            var nameWithoutNoun = name.Split("/").First();
+        
+            var soldierBonusTypes = CommendationNameToSoldierBonusTypesMap[nameWithoutNoun];
+            var soldierBonusType = soldierBonusTypes[decoration];
+            var statsData = SoldierBonusNameToStatsMap[soldierBonusType];
+            soldierBonuses.Add(statsData);
+        }
 
         // kja commented out as I need to support soldierTransformations file / soldierBonusType. See for example STR_COMBAT_PILOT_TRAINING
         // foreach (string name in soldier.TransformationBonuses.TransformationNames)
