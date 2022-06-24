e1b9d590fc8faca1975da5ec849c917f4d8cc0e4
Konrad Jamrozik <kojamroz@microsoft.com>
2022-06-24 03:05:20 -0700

    fix mana stat caps

--- oxce-tests/StatCaps.cs
+++ oxce-tests/StatCaps.cs
@@ -17,10 +17,10 @@ namespace OxceTests
         int Mana)
     {
         // @formatter:off
-        private static readonly StatCaps SoldierStatCaps  = new StatCaps(65, 90, 45, 100, 60, 90, 70, 50, 100, 50, 90, 100);
-        private static readonly StatCaps HybridStatCaps   = new StatCaps(65, 70, 40, 100, 70, 90, 70, 40, 90, 90, 90, 80);
+        private static readonly StatCaps SoldierStatCaps  = new StatCaps(65, 90, 45, 100, 60, 90, 70, 50, 100, 50, 90, 120);
+        private static readonly StatCaps HybridStatCaps   = new StatCaps(65, 70, 40, 100, 70, 90, 70, 40, 90, 90, 90, 120);
         private static readonly StatCaps DogStatCaps      = new StatCaps(100, 130, 50, 40, 110, 10, 10, 2, 40, 0, 140, 220);
-        private static readonly StatCaps RatStatCaps      = new StatCaps(65, 100, 18, 30, 80, 0, 0, 2, 45, 0, 60, 80);
+        private static readonly StatCaps RatStatCaps      = new StatCaps(65, 100, 18, 30, 80, 0, 0, 2, 45, 0, 60, 120);
         private static readonly StatCaps MuggleAIStatCaps = new StatCaps(75, 90, 15, 0, 50, 100, 80, 0, 0, 0, 80, 100);
         // @formatter:on
 
