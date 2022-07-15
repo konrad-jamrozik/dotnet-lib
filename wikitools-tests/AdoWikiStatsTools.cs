00345122fafe4db1f381e11920da93f69abd53ad
Konrad Jamrozik <kojamroz@microsoft.com>
2022-07-14 19:31:00 -0700

    lay out the attack plan for ToolTransferStatsFilesIntoMonthlyStorage

---
+++ wikitools-tests/AdoWikiStatsTools.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Threading.Tasks;
+using Wikitools.AzureDevOps;
+using Wikitools.Config;
+using Wikitools.Lib.Configuration;
+using Wikitools.Lib.Json;
+using Wikitools.Lib.OS;
+using Wikitools.Lib.Primitives;
+using Wikitools.Lib.Storage;
+using Xunit;
+using Environment = Wikitools.Lib.OS.Environment;
+
+namespace Wikitools.Tests;
+
+public class AdoWikiStatsTools
+{
+    private string StatsFileName(DateTime dateTime, int pageViewsForDays)
+        => $"wiki_stats_{dateTime:yyyy_MM_dd}_{pageViewsForDays}days.json";
+
+    [Fact(Skip = "Tool to be used manually")]
+    public async Task ToolGetWikiStats()
+    {
+        ITimeline    timeline = new Timeline();
+        IFileSystem  fs       = new FileSystem();
+        IEnvironment env      = new Environment();
+        IWikitoolsCfg cfg     = new Configuration(fs).Load<IWikitoolsCfg>();
+        IAdoWiki wiki = new AdoWiki(
+            cfg.AzureDevOpsCfg().AdoWikiUri(),
+            cfg.AzureDevOpsCfg().AdoPatEnvVar(),
+            env);
+
+        var pagesViewsStats = wiki.PagesStats(PageViewsForDays.Max);
+
+        var storage = new MonthlyJsonFilesStorage(new Dir(fs, cfg.StorageDirPath()));
+
+        await storage.Write(await pagesViewsStats,
+            new DateMonth(timeline.UtcNow),
+            StatsFileName(timeline.UtcNow, cfg.AdoWikiPageViewsForDays()));
+    }
+    
+    [Fact]
+    public void ToolTransferStatsFilesIntoMonthlyStorage()
+    {
+        // kja curr work
+        // 1. obtain references to all wiki_stats_ files in given directory
+        //
+        //    // This needs to parse not only the file contents, but also data from file name: date and int.
+        //    var statsFiles = GetAllFilesInDir(dir: cfg.StorageDirPath() filePattern: regex for wiki_stats_[yyyy_MM_dd]_[int]days.json")
+        //
+        // 2. deserialize stats from these files
+        //
+        //    List<ValidWikiPagesStats> stats = statsFiles.Select(file => DeserializeStats(file, GetDaySpan(file.date, file.int)))
+        //    
+        // 3. merge the stats into one huge valid wiki stats
+        //
+        //    ValidWikiPagesStats mergedStats = ValidWikiPagesStats.Merge(stats);
+        //
+        // 4. partition them by month, in preparation for each month to be
+        // saved to corresponding monthly stats file
+        //
+        //    // See also https://morelinq.github.io/2.4/ref/api/html/M_MoreLinq_MoreEnumerable_Partition__2_2.htm
+        //    List<ValidWikiPagesStats> statsByMonth = mergedStats.PartitionByMonth()
+        //
+        // 5. for each month, compare the stats-about-to-be-saved with the
+        // the already saved monthly stats, if any.
+        // 5.1. If they differ, then report this to stdout. If dryRun=false, override.
+        //
+        //    var statsDiffs = statsByMonth.ForEach(stats => new JsonDiff(monthlyStatsStorage.Read(stats.Month), stats))
+        //    var nonemptyDiffs = statsDiffs.Where(diff => diff.IsNonempty)
+        //    nonemptyDiffs.ForEach(diff => LogToStdout(diff);
+        //    if (dryRun == false)
+        //      // Note that here actually NewStatsForTheDiff method is bogus; instead, when computing the diffs
+        //      // in the first place, they need to stay correlated with the stats, e.g. in a dictionary.
+        //      nonemptyDiffs.ForEach(diff => storage.Write(NewStatsForTheDiff(diff)))
+        //
+        // ------------------------------------------
+        // Alternative mode: when ToolGetWikiStats runs, execute this logic,
+        // but only for the most recent two months (up to thePageViewsForDays.Max)
+        //
+        // Note:
+        // The transfer is from files like wiki_stats_2022_07_02_30days.json
+        // to files like date_2022_07.json
+        // (format from Wikitools.Lib.Storage.MonthlyJsonFilesStorage.FileName)
+    }
+
+    [Fact(Skip = "Tool to be used manually")]
+    public void SplitIntoMonthlyStats()
+    {
+        IFileSystem fs = new FileSystem();
+        var cfg = new Configuration(fs).Load<IWikitoolsCfg>();
+        var storage = new MonthlyJsonFilesStorage(new Dir(fs, cfg.StorageDirPath()));
+
+        var data = new (int month, (int month, int day) stats1, (int month, int day) stats2)[]
+        {
+            (month: 1, (month: 1, day: 19), (month: 2, day: 7)),
+            (month: 2, (month: 2, day: 7), (month: 3, day: 3)),
+            (month: 3, (month: 3, day: 28), (month: 4, day: 26)),
+            (month: 4, (month: 4, day: 26), (month: 5, day: 4)),
+            (month: 5, (month: 5, day: 14), (month: 6, day: 4)),
+            (month: 6, (month: 6, day: 19), (month: 7, day: 15)),
+            (month: 7, (month: 7, day: 15), (month: 8, day: 3)),
+            (month: 8, (month: 8, day: 13), (month: 9, day: 6)),
+            (month: 9, (month: 9, day: 24), (month: 10, day: 23)),
+            (month: 10, (month: 10, day: 23), (month: 11, day: 14)),
+            (month: 11, (month: 11, day: 14), (month: 11, day: 28))
+        };
+
+        foreach (var (month, stats1, stats2) in data)
+        {
+            MergeWikiStatsIntoMonth(
+                fs,
+                cfg.StorageDirPath(),
+                storage,
+                new DateDay(2021, stats1.month, stats1.day, DateTimeKind.Utc),
+                new DateDay(2021, stats2.month, stats2.day, DateTimeKind.Utc),
+                new DateMonth(2021, month, DateTimeKind.Utc));
+        }
+    }
+
+    private void MergeWikiStatsIntoMonth(
+        IFileSystem fs,
+        string storageDirPath,
+        MonthlyJsonFilesStorage storage,
+        DateDay stats1EndDay,
+        DateDay stats2EndDay,
+        DateMonth outputMonth)
+    {
+        var stats1Path = storageDirPath + $"/wiki_stats_{stats1EndDay:yyyy_MM_dd}_30days.json";
+        var stats1StartDay = stats1EndDay.AddDays(-29);
+        var stats1 = DeserializeStats(fs, (stats1Path, new DaySpan(stats1StartDay, stats1EndDay)));
+
+        var stats2Path = storageDirPath + $"/wiki_stats_{stats2EndDay:yyyy_MM_dd}_30days.json";
+        var stats2StartDay = stats2EndDay.AddDays(-29);
+        var stats2 = DeserializeStats(fs, (stats2Path, new DaySpan(stats2StartDay, stats2EndDay)));
+
+        var mergedStats = ValidWikiPagesStats.Merge(new[] { stats1, stats2 });
+
+        storage.Write(
+            mergedStats.Trim(outputMonth),
+            outputMonth,
+            // kja this duplicates Wikitools.Lib.Storage.MonthlyJsonFilesStorage.FileName
+            $"date_{outputMonth:yyyy_MM}.json").Wait();
+    }
+
+    private static ValidWikiPagesStats DeserializeStats(
+        IFileSystem fs,
+        (string statsPath, DaySpan daySpan) statsData)
+        => new ValidWikiPagesStats(
+            fs.ReadAllText(statsData.statsPath).FromJsonTo<WikiPageStats[]>(),
+            statsData.daySpan);
+}
--- wikitools-tests/Tools.cs
+++ wikitools-tests/Tools.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.Services.CircuitBreaker;
 using Wikitools.AzureDevOps;
 using Wikitools.Config;
 using Wikitools.Lib.Configuration;
-using Wikitools.Lib.Json;
 using Wikitools.Lib.OS;
 using Wikitools.Lib.Primitives;
-using Wikitools.Lib.Storage;
 using Xunit;
 using Xunit.Abstractions;
-using Environment = Wikitools.Lib.OS.Environment;
 
 namespace Wikitools.Tests;
 
@@ -22,7 +19,7 @@ public class Tools
         _testOut = testOut;
     }
 
-    [Fact]//(Skip = "For one-off experiments")]
+    [Fact(Skip = "For one-off experiments")]
     public void Scratchpad()
     {
         DaySpan x = new DaySpan(new DateDay(DateTime.UtcNow), new DateDay(DateTime.UtcNow));
@@ -44,101 +41,4 @@ public class Tools
             _testOut.WriteLine(fileTreePath);
         }
     }
-
-    [Fact(Skip = "Tool to be used manually")]
-    public async Task ToolGetWikiStats()
-    {
-        ITimeline    timeline = new Timeline();
-        IFileSystem  fs       = new FileSystem();
-        IEnvironment env      = new Environment();
-        IWikitoolsCfg cfg     = new Configuration(fs).Load<IWikitoolsCfg>();
-        IAdoWiki wiki = new AdoWiki(
-            cfg.AzureDevOpsCfg().AdoWikiUri(),
-            cfg.AzureDevOpsCfg().AdoPatEnvVar(),
-            env);
-
-        var pagesViewsStats = wiki.PagesStats(PageViewsForDays.Max);
-
-        var storage = new MonthlyJsonFilesStorage(new Dir(fs, cfg.StorageDirPath()));
-
-        await storage.Write(await pagesViewsStats,
-            new DateMonth(timeline.UtcNow),
-            $"wiki_stats_{timeline.UtcNow:yyyy_MM_dd}_{cfg.AdoWikiPageViewsForDays()}days.json");
-    }
-
-    // kja I need to ensure this transferring of data scraped from ADO wiki into monthly storage
-    // is done at the end of ToolGetWikiStats(); two modes:
-    // a) the transfer will happen for thePageViewsForDays.Max in normal run, and
-    // b) in a separate tool, re-merge all stats, check if the merged stats correspond to the saves stats,
-    // show diff in not.
-    //
-    // The transfer is from files like wiki_stats_2022_07_02_30days.json
-    // to files like date_2022_07.json
-    // (format from Wikitools.Lib.Storage.MonthlyJsonFilesStorage.FileName)
-    [Fact(Skip = "Tool to be used manually")]
-    public void SplitIntoMonthlyStats()
-    {
-        IFileSystem fs = new FileSystem();
-        var cfg = new Configuration(fs).Load<IWikitoolsCfg>();
-        var storage = new MonthlyJsonFilesStorage(new Dir(fs, cfg.StorageDirPath()));
-
-        var data = new (int month, (int month, int day) stats1, (int month, int day) stats2)[]
-        {
-            (month: 1, (month: 1, day: 19), (month: 2, day: 7)),
-            (month: 2, (month: 2, day: 7), (month: 3, day: 3)),
-            (month: 3, (month: 3, day: 28), (month: 4, day: 26)),
-            (month: 4, (month: 4, day: 26), (month: 5, day: 4)),
-            (month: 5, (month: 5, day: 14), (month: 6, day: 4)),
-            (month: 6, (month: 6, day: 19), (month: 7, day: 15)),
-            (month: 7, (month: 7, day: 15), (month: 8, day: 3)),
-            (month: 8, (month: 8, day: 13), (month: 9, day: 6)),
-            (month: 9, (month: 9, day: 24), (month: 10, day: 23)),
-            (month: 10, (month: 10, day: 23), (month: 11, day: 14)),
-            (month: 11, (month: 11, day: 14), (month: 11, day: 28))
-        };
-
-        foreach (var (month, stats1, stats2) in data)
-        {
-            MergeWikiStatsIntoMonth(
-                fs,
-                cfg.StorageDirPath(),
-                storage,
-                new DateDay(2021, stats1.month, stats1.day, DateTimeKind.Utc),
-                new DateDay(2021, stats2.month, stats2.day, DateTimeKind.Utc),
-                new DateMonth(2021, month, DateTimeKind.Utc));
-        }
-    }
-
-    private void MergeWikiStatsIntoMonth(
-        IFileSystem fs,
-        string storageDirPath,
-        MonthlyJsonFilesStorage storage,
-        DateDay stats1EndDay,
-        DateDay stats2EndDay,
-        DateMonth outputMonth)
-    {
-        var stats1Path = storageDirPath + $"/wiki_stats_{stats1EndDay:yyyy_MM_dd}_30days.json";
-        var stats1StartDay = stats1EndDay.AddDays(-29);
-        var stats1 = DeserializeStats(fs, (stats1Path, new DaySpan(stats1StartDay, stats1EndDay)));
-
-        var stats2Path = storageDirPath + $"/wiki_stats_{stats2EndDay:yyyy_MM_dd}_30days.json";
-        var stats2StartDay = stats2EndDay.AddDays(-29);
-        var stats2 = DeserializeStats(fs, (stats2Path, new DaySpan(stats2StartDay, stats2EndDay)));
-
-        var mergedStats = ValidWikiPagesStats.Merge(new[] { stats1, stats2 });
-
-        storage.Write(
-            mergedStats.Trim(outputMonth),
-            outputMonth,
-            // kja this duplicates Wikitools.Lib.Storage.MonthlyJsonFilesStorage.FileName
-            $"date_{outputMonth:yyyy_MM}.json").Wait();
-    }
-
-    private static ValidWikiPagesStats DeserializeStats(
-        IFileSystem fs,
-        (string statsPath, DaySpan daySpan) statsData) =>
-        new(
-            fs.ReadAllText(statsData.statsPath)
-                .FromJsonTo<WikiPageStats[]>(),
-            statsData.daySpan);
 }
