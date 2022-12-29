77275497eb23e9c95da4781a362ba479e21fe098
Konrad Jamrozik <kojamroz@microsoft.com>
2022-12-29 02:11:46 -0800

    simplify: SoldiersToSend doesn't need to call Refresh in setter, now that parent-child @bind works as expected.

--- ufo-game/Model/MissionPrep.cs
+++ ufo-game/Model/MissionPrep.cs
@@ -2,21 +2,5 @@
 
 public class MissionPrep
 {
-    private readonly StateRefresh _refresh;
-    private int _soldiersToSend;
-
-    public MissionPrep(StateRefresh refresh)
-    {
-        _refresh = refresh;
-    }
-
-    public int SoldiersToSend
-    {
-        get => _soldiersToSend;
-        set
-        {
-            _soldiersToSend = value;
-            _refresh.Refresh();
-        }
-    }
+    public int SoldiersToSend { get; set; }
 }
