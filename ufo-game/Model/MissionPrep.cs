9a7e6f522d25a3921b6a13626644e75603a72f68
Konrad Jamrozik <kojamroz@microsoft.com>
2022-12-29 00:25:41 -0800

    use CardInput component, made possible by adding support to properly updating state on change to SoldiersToSend

--- ufo-game/Model/MissionPrep.cs
+++ ufo-game/Model/MissionPrep.cs
@@ -2,5 +2,21 @@
 
 public class MissionPrep
 {
-    public int SoldiersToSend = 0;
+    private readonly StateRefresh _refresh;
+    private int _soldiersToSend;
+
+    public MissionPrep(StateRefresh refresh)
+    {
+        _refresh = refresh;
+    }
+
+    public int SoldiersToSend
+    {
+        get => _soldiersToSend;
+        set
+        {
+            _soldiersToSend = value;
+            _refresh.Refresh();
+        }
+    }
 }
--- ufo-game/Model/StateRefresh.cs
+++ ufo-game/Model/StateRefresh.cs
@@ -1,5 +1,12 @@
 ﻿namespace UfoGame.Model;
 
+/// <summary>
+/// Implements "In-memory container service" pattern:
+/// https://learn.microsoft.com/en-us/aspnet/core/blazor/state-management?view=aspnetcore-7.0&pivots=server#in-memory-state-container-service-server
+///
+/// See also:
+/// https://learn.microsoft.com/en-us/aspnet/core/blazor/components/rendering?view=aspnetcore-7.0#to-render-a-component-outside-the-subtree-thats-rerendered-by-a-particular-event
+/// </summary>
 public class StateRefresh
 {
     public Action Refresh = () =>
