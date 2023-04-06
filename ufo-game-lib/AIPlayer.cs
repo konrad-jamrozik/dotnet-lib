17a88aae174533136c15a75443df439da329acc0
Konrad Jamrozik <kojamroz@microsoft.com>
2023-04-06 16:24:52 -0700

    lay out plan for first AIPlayer implementation

--- ufo-game-lib/AIPlayer.cs
+++ ufo-game-lib/AIPlayer.cs
@@ -13,6 +13,16 @@ public class AIPlayer
 
     public void PlayGame()
     {
-        // kja to implement
+        // kja to implement AI Player
+        // First level:
+        // - Advance time until mission available
+        // - Once mission available, hire agents up to transport limit and send on mission
+        // - Repeat until player loses game (at this point impossible to win)
+        // 
+        // Next level:
+        // - Try to always keep at least enough agents to maintain full transport capacity
+        // - Send agents on intel-gathering duty until mission is available
+        // - Send agents on available mission if not too hard
+        // - Do not hire agents if it would lead to bankruptcy
     }
 }
