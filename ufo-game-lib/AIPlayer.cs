e5aa36ccd17a1c4b15f445cb2d1a08668bd3e4e5
Konrad Jamrozik <kojamroz@microsoft.com>
2023-04-06 16:10:18 -0700

    TDD implement test stub: AIPlayerTests.AIPlayerPlaysGameUntilConclusion

--- ufo-game-lib/AIPlayer.cs
+++ ufo-game-lib/AIPlayer.cs
@@ -1,6 +1,18 @@
+using UfoGameLib.Infra;
+
 namespace UfoGameLib;
 
 public class AIPlayer
 {
+    private readonly GameSessionController _game;
 
+    public AIPlayer(GameSessionController game)
+    {
+        _game = game;
+    }
+
+    public void PlayGame()
+    {
+        // kja to implement
+    }
 }
