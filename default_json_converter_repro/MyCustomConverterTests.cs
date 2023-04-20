ddd8a2e1277d8cc6aab2b7aa562f0d01e6f41482
Konrad Jamrozik <kojamroz@microsoft.com>
2023-04-20 02:25:58 -0700

    simplify

--- default_json_converter_repro/MyCustomConverterTests.cs
+++ default_json_converter_repro/MyCustomConverterTests.cs
@@ -12,29 +12,29 @@ public class MyCustomConverterTests
         var employee = new Employee();
 
         // This is going to throw NRE on s_defaultConverter.Write(writer, value, options);
-        string serialized = JsonSerializer.Serialize(employee, new JsonSerializerOptions
-        {
-            Converters = { new MyCustomConverter() }
-        });
+        string serialized = JsonSerializer.Serialize(
+            employee,
+            new JsonSerializerOptions
+            {
+                Converters = { new MyCustomConverter() },
+            });
         Console.Out.WriteLine("serialized: " + serialized);
     }
 }
 
 public class MyCustomConverter : JsonConverter<Employee>
 {
-    private readonly static JsonConverter<Employee> s_defaultConverter = 
+    private readonly static JsonConverter<Employee> s_defaultConverter =
         (JsonConverter<Employee>)JsonSerializerOptions.Default.GetConverter(typeof(Employee));
 
     // Custom serialization logic
-    public override void Write(
-        Utf8JsonWriter writer, Employee value, JsonSerializerOptions options)
+    public override void Write(Utf8JsonWriter writer, Employee value, JsonSerializerOptions options)
     {
-            s_defaultConverter.Write(writer, value, options);
+        s_defaultConverter.Write(writer, value, options);
     }
 
     // Fall back to default deserialization logic
-    public override Employee Read(
-        ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override Employee Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         return s_defaultConverter.Read(ref reader, typeToConvert, options)!;
     }
@@ -42,7 +42,4 @@ public class MyCustomConverter : JsonConverter<Employee>
 
 public class Employee
 {
-    public string? Name { get; set; }
-    public Employee? Manager { get; set; }
-    public List<Employee>? DirectReports { get; set; }
 }
