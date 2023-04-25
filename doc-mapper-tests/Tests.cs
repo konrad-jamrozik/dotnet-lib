8f7ff01146652a6681ac8e51a6a7a639a2edc877
Konrad Jamrozik <kojamroz@microsoft.com>
2023-04-25 01:01:56 -0700

    DocMapper: work on implementation

--- doc-mapper-tests/Tests.cs
+++ doc-mapper-tests/Tests.cs
@@ -1,11 +1,13 @@
-using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace DocMapper;
 
 public class Tests
 {
+    public const int MaxExplorationDepth = 5;
+
     public const string RepoClonePathHomeDirSuffixAzureRestApiSpecs = "/repos/azure-rest-api-specs/";
+    public const string RepoClonePathHomeDirInternalWiki = "/repos/internal.wiki/";
 
     public const string MapEntryPointUrl =
         "https://github.com/Azure/azure-rest-api-specs/blob/main/.github/PULL_REQUEST_TEMPLATE/control_plane_template.md";
@@ -24,10 +26,21 @@ public class Tests
     public HashSet<string> ExploredUrls = new HashSet<string>() {};
     public HashSet<string> SkippedUrls = new HashSet<string>() {};
 
+    // See also: https://learn.microsoft.com/en-us/rest/api/azure/devops/wiki/pages/get-page?view=azure-devops-rest-7.0&tabs=HTTP
+    // kja TODO: write a separate test that will get all these URLs from the API and dump them to a local file to be read in Setup.
+    // This might need to be done for multiple wikis.
+    public readonly Dictionary<string, string> InternalWikiUrlPagePathToClonePathMap = new Dictionary<string, string>()
+    {
+        // https://dev.azure.com/azure-sdk/internal/_wiki/wikis/internal.wiki/208/OpenAPI-Hub-Adding-new-API-version
+        ["208/OpenAPI-Hub-Adding-new-API-version"] =
+            "Welcome-Service-Partners!/Management-Plane-SDKs-%2D-service-and-product-team-docs/OpenAPI-Hub%3A-Adding-new-API-version.md"
+    };
+
     [SetUp]
     public void Setup()
     {
         string homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        
         string repoClonePathAzureRestApiSpecs =
             Path.Join(homeDir, RepoClonePathHomeDirSuffixAzureRestApiSpecs).Replace("\\", "/");
         
@@ -36,9 +49,18 @@ public class Tests
             Is.True,
             $"Directory.Exists(repoClonePathAzureRestApiSpecs={repoClonePathAzureRestApiSpecs})");
 
+        string repoClonePathInternalWiki =
+            Path.Join(homeDir, RepoClonePathHomeDirInternalWiki).Replace("\\", "/");
+        
+        Assume.That(
+            Directory.Exists(repoClonePathInternalWiki),
+            Is.True,
+            $"Directory.Exists(repoClonePathInternalWiki={repoClonePathInternalWiki})");
+
         UrlPrefixToClonePathMap = new Dictionary<string, string>
         {
-            ["https://github.com/Azure/azure-rest-api-specs/blob/main/"] = repoClonePathAzureRestApiSpecs
+            ["https://github.com/Azure/azure-rest-api-specs/blob/main/"] = repoClonePathAzureRestApiSpecs,
+            ["https://dev.azure.com/azure-sdk/internal/_wiki/wikis/internal.wiki/"] = repoClonePathInternalWiki
         };
     }
 
@@ -47,10 +69,9 @@ public class Tests
     {
         UrlsToExplore.Enqueue(MapEntryPointUrl);
 
-        int maxExplorationDepth = 2;
-        int currExplorationDepth = 1;
+        int currExplorationDepth = 0;
 
-        while (UrlsToExplore.Count > 0 && currExplorationDepth <= maxExplorationDepth)
+        while (UrlsToExplore.Count > 0 && currExplorationDepth < MaxExplorationDepth)
         {
             string urlToExplore = UrlsToExplore.Dequeue();
             Console.Out.WriteLine($"Exploring URL: {urlToExplore}");
@@ -91,7 +112,8 @@ public class Tests
                 continue;
             }
 
-            string? matchingClone = UrlPrefixToClonePathMap.Keys.SingleOrDefault(repoClonePath => urlToExplore.StartsWith(repoClonePath));
+            string? matchingClone =
+                UrlPrefixToClonePathMap.Keys.SingleOrDefault(repoClonePath => urlToExplore.StartsWith(repoClonePath));
             if (matchingClone == null)
             {
                 Console.WriteLine(
@@ -100,21 +122,42 @@ public class Tests
                 continue;
             }
 
-            ExploreUrl(urlToExplore, matchingClone);
+            string filePathInLocalClone = urlToExplore.Replace(matchingClone, UrlPrefixToClonePathMap[matchingClone]);
+
+            if (filePathInLocalClone.Contains("internal.wiki"))
+            {
+                string? wikiUrlPagePath = InternalWikiUrlPagePathToClonePathMap.Keys.SingleOrDefault(wikiUrlPagePath => filePathInLocalClone.EndsWith(wikiUrlPagePath));
+                if (wikiUrlPagePath == null)
+                {
+                    Console.WriteLine(
+                        $"No known translation to clone .md path available for given internal.wiki page URL path. filePathInLocalClone={filePathInLocalClone}");
+                    SkippedUrls.Add(urlToExplore);
+                    continue;
+                }
+                else
+                {
+                    filePathInLocalClone = filePathInLocalClone.Replace(
+                        wikiUrlPagePath,
+                        InternalWikiUrlPagePathToClonePathMap[wikiUrlPagePath]);
+                }
+            }
+
+
+            Assert.IsTrue(File.Exists(filePathInLocalClone), $"File.Exists(FilePathInLocalClone={filePathInLocalClone})");
+
+            ExploreUrlsInFile(filePathInLocalClone);
 
             ExploredUrls.Add(urlToExplore);
             currExplorationDepth++;
         }
 
+        Console.Out.WriteLine($"Exploration loop terminated. Exploration depth reached: {currExplorationDepth}/{MaxExplorationDepth}");
+
         Assert.Pass();
     }
 
-    private void ExploreUrl(string urlToExplore, string matchingClone)
+    private void ExploreUrlsInFile(string filePathInLocalClone)
     {
-        string filePathInLocalClone = urlToExplore.Replace(matchingClone!, UrlPrefixToClonePathMap[matchingClone!]);
-
-        Assert.IsTrue(File.Exists(filePathInLocalClone), $"File.Exists(FilePathInLocalClone={filePathInLocalClone})");
-
         string fileAllText = File.ReadAllText(filePathInLocalClone);
 
         List<string> hyperlinkUrls = ExtractHyperlinkUrls(fileAllText);
@@ -146,6 +189,8 @@ public class Tests
         // Regex patterns to match markdown hyperlinks
         string inlinePattern = @"\[(?:[^\[\]]*)\]\((.*?)\)"; // Inline links: [text](URL)
         string referencePattern = @"\[(?:[^\[\]]+)\]:\s*(.+)"; // Reference links: [text]: URL
+        // kja TODO: need to handle relative wiki-internal URLS, as seen e.g. here:
+        // https://dev.azure.com/azure-sdk/internal/_wiki/wikis/internal.wiki/208/OpenAPI-Hub-Adding-new-API-version
 
         // Match collections for both inline and reference hyperlinks
         MatchCollection inlineMatches = Regex.Matches(markdownContent, inlinePattern);
