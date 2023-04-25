3b903067b59ac025ef06365f36c4812f806265a5
Konrad Jamrozik <kojamroz@microsoft.com>
2023-04-25 00:36:49 -0700

    DocMapper: implement the URL exploration loop.

--- doc-mapper-tests/Tests.cs
+++ doc-mapper-tests/Tests.cs
@@ -17,7 +17,12 @@ public class Tests
         ["https://aka.ms/openapiportal"] = "https://portal.azure-devex-tools.com/"
     };
 
-    public HashSet<string> LeafAliases = new HashSet<string>() { "https://portal.azure-devex-tools.com/" };
+    public HashSet<string> LeafUrls = new HashSet<string>() { "https://portal.azure-devex-tools.com/" };
+
+    public Queue<string> UrlsToExplore = new Queue<string>() {};
+
+    public HashSet<string> ExploredUrls = new HashSet<string>() {};
+    public HashSet<string> SkippedUrls = new HashSet<string>() {};
 
     [SetUp]
     public void Setup()
@@ -40,11 +45,73 @@ public class Tests
     [Test]
     public void MapsDocs()
     {
-        string? matchingKey = UrlPrefixToClonePathMap.Keys.SingleOrDefault(key => MapEntryPointUrl.StartsWith(key));
-        if (matchingKey == null)
-            Assert.Fail($"No repo clone path matched to a prefix of MapEntryPointUrl={MapEntryPointUrl}");
+        UrlsToExplore.Enqueue(MapEntryPointUrl);
 
-        string filePathInLocalClone = MapEntryPointUrl.Replace(matchingKey!, UrlPrefixToClonePathMap[matchingKey!]);
+        int maxExplorationDepth = 2;
+        int currExplorationDepth = 1;
+
+        while (UrlsToExplore.Count > 0 && currExplorationDepth <= maxExplorationDepth)
+        {
+            string urlToExplore = UrlsToExplore.Dequeue();
+            Console.Out.WriteLine($"Exploring URL: {urlToExplore}");
+            Console.Out.WriteLine($"----------------------------------------");
+
+            if (SkippedUrls.Contains(urlToExplore))
+            {
+                Console.WriteLine($"Already skipped exploring urlToExplore={urlToExplore}. Skipping exploring the URL (again).");
+                continue;
+            }
+
+            if (urlToExplore.StartsWith("https://aka.ms"))
+            {
+                string? matchingAkaMsAlias = UrlAliases.Keys.SingleOrDefault(akaMsAlias => urlToExplore.StartsWith(akaMsAlias));
+                if (matchingAkaMsAlias == null)
+                {
+                    Console.WriteLine(
+                        $"No aka.ms alias matched to a prefix of urlToExplore={urlToExplore}. Skipping exploring the URL.");
+                    SkippedUrls.Add(urlToExplore);
+                    continue;
+                }
+                else
+                {
+                    urlToExplore = UrlAliases[matchingAkaMsAlias];
+                }
+            }
+
+            if (ExploredUrls.Contains(urlToExplore))
+            {
+                Console.WriteLine($"Already explored urlToExplore={urlToExplore}. Skipping exploring the URL.");
+                continue;
+            }
+
+            if (LeafUrls.Contains(urlToExplore))
+            {
+                Console.WriteLine($"urlToExplore={urlToExplore} is a leaf URL. Skipping exploring the URL.");
+                SkippedUrls.Add(urlToExplore);
+                continue;
+            }
+
+            string? matchingClone = UrlPrefixToClonePathMap.Keys.SingleOrDefault(repoClonePath => urlToExplore.StartsWith(repoClonePath));
+            if (matchingClone == null)
+            {
+                Console.WriteLine(
+                    $"No repo clone path matched to a prefix of urlToExplore={urlToExplore}. Skipping exploring the URL.");
+                SkippedUrls.Add(urlToExplore);
+                continue;
+            }
+
+            ExploreUrl(urlToExplore, matchingClone);
+
+            ExploredUrls.Add(urlToExplore);
+            currExplorationDepth++;
+        }
+
+        Assert.Pass();
+    }
+
+    private void ExploreUrl(string urlToExplore, string matchingClone)
+    {
+        string filePathInLocalClone = urlToExplore.Replace(matchingClone!, UrlPrefixToClonePathMap[matchingClone!]);
 
         Assert.IsTrue(File.Exists(filePathInLocalClone), $"File.Exists(FilePathInLocalClone={filePathInLocalClone})");
 
@@ -52,12 +119,24 @@ public class Tests
 
         List<string> hyperlinkUrls = ExtractHyperlinkUrls(fileAllText);
 
-        foreach (var hyperlinkUrl in hyperlinkUrls)
+        foreach (string hyperlinkUrl in hyperlinkUrls)
         {
             Console.Out.WriteLine(hyperlinkUrl);
         }
+        Console.Out.WriteLine($"========================================");
 
-        Assert.Pass();
+        foreach (string hyperlinkUrl in hyperlinkUrls)
+        {
+            if (LeafUrls.Contains(hyperlinkUrl) || ExploredUrls.Contains(hyperlinkUrl) ||
+                SkippedUrls.Contains(hyperlinkUrl))
+            {
+                // Do nothing - the URL is a leaf URL, already explored, or already skipped from exploration.
+            }
+            else
+            {
+                UrlsToExplore.Enqueue(hyperlinkUrl);
+            }
+        }
     }
 
     public static List<string> ExtractHyperlinkUrls(string markdownContent)
